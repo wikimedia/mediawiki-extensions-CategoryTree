a43db437c793d171ec58ee02765447db108455e7
Rotem Liss <rotem@users.mediawiki.org>
2006-09-01 17:30:38 +0000

    CategoryTree he localisation.

---
+++ CategoryTree.i18n.he.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
+
+$messages['categorytree']= 'עץ קטגוריות';
+$messages['categorytree-tab']= 'עץ';
+$messages['categorytree-header']= 
+'הקלידו שם קטגוריה כדי לראות את תכניה במבנה עץ. שימו לב שהדבר דורש תכונת JavaScript מתקדמת, הידועה בשם AJAX. אם יש לכם דפדפן ישן מאוד, או ש־JavaScript מנוטרלת אצלכם בדפדפן, הוא לא יעבוד.
+';
+
+$messages['categorytree-category']= 'קטגוריה';
+$messages['categorytree-go']= 'הצג את העץ';
+$messages['categorytree-parents']= 'הורים';
+
+$messages['categorytree-mode-categories']= 'קטגוריות בלבד';
+$messages['categorytree-mode-pages']= 'דפים שאינם תמונות';
+$messages['categorytree-mode-all']= 'כל הדפים';
+
+$messages['categorytree-collapse']= 'כיווץ';
+$messages['categorytree-expand']= 'הרחבה';
+$messages['categorytree-load']= 'טעינה';
+$messages['categorytree-loading']= 'בטעינה';
+$messages['categorytree-nothing-found']= 'לא נמצאו תוצאות';
+$messages['categorytree-not-found']= "הקטגוריה '''$1''' לא נמצאה";
+
+$messages['categorytree-show-list'] = "תצוגת רשימה";
+$messages['categorytree-show-tree'] = "תצוגת עץ";
+$messages['categorytree-too-many-subcats'] = "לא ניתן להציג את קטגוריות המשנה כעץ, כיוון שהן מרובות מדי.";
+
+?>
