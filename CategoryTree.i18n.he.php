707b65b6d19ce6f969c3633712f6839fd830cac1
Rotem Liss <rotem@users.mediawiki.org>
2006-09-03 13:38:36 +0000

    Update.

--- CategoryTree.i18n.he.php
+++ CategoryTree.i18n.he.php
@@ -25,7 +25,7 @@ $messages['categorytree-collapse']= 'כיווץ';
 $messages['categorytree-expand']= 'הרחבה';
 $messages['categorytree-load']= 'טעינה';
 $messages['categorytree-loading']= 'בטעינה';
-$messages['categorytree-nothing-found']= 'לא נמצאו תוצאות';
+$messages['categorytree-nothing-found']= 'אין קטגוריות משנה';
 $messages['categorytree-not-found']= "הקטגוריה '''$1''' לא נמצאה";
 
 $messages['categorytree-show-list'] = "תצוגת רשימה";
