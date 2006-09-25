753fd138c43cdefa760a9c0e0f2ee052e7885848
Rotem Liss <rotem@users.mediawiki.org>
2006-09-25 17:49:42 +0000

    Updates and additions for the Dutch (nl) translation of many extensions, sent by Siebrand.

--- CategoryTree.i18n.nl.php
+++ CategoryTree.i18n.nl.php
@@ -1,57 +1,42 @@
 <?php
-/*
-  Internationalisation file for the CategoryTree extension
-  Copyright (C) 2006, Daniel Kinzler, brightbyte.de;
-  Dutch translation by JePe and Siebrand
- 
-  This file is licensed under the GPL and the GFDL:
-  
-  
-  Permission is granted to copy, distribute and/or modify this document
-  under the terms of the GNU Free Documentation License, Version 1.2
-  or any later version published by the Free Software Foundation;
-  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
-  Texts.  A copy of the license is included in the section entitled "GNU
-  Free Documentation License".
-  
-  
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-  */
-  
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Dutch translation by JePe and Siebrand, source file by Daniel Kinzler <duesentrieb@brightbyte.de>
+ * @copyright © 2006 Daniel Kinzler, JePe, Siebrand
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
 $messages['categorytree']= 'Categorieboom';
 $messages['categorytree-tab']= 'Boom';
-$messages['categorytree-header']= 
+$messages['categorytree-header']=
 'Geef een categorienaam in om de inhoud als een boomstructuur te bekijken.
 Let op: deze functie gebruikt [[:nl:JavaScript|JavaScript]]-functionaliteit die bekend staat als [[:nl:Ajax (webdesign)|AJAX]].
 Als u een erg verouderde browser heeft of JavaScript staat uitgeschakeld, dan werkt deze functie niet.
 ';
-  
+
 $messages['categorytree-category']= 'Categorie';
 $messages['categorytree-go']= 'Laden';
 $messages['categorytree-parents']= 'Bovenliggende categorieën';
-  
+
 $messages['categorytree-mode-categories']= 'alleen categorieën';
 $messages['categorytree-mode-pages']= 'geen afbeeldingen';
 $messages['categorytree-mode-all']= 'alle pagina\'s';
-  
+
 $messages['categorytree-collapse']= 'inklappen';
 $messages['categorytree-expand']= 'uitklappen';
 $messages['categorytree-load']= 'laden';
 $messages['categorytree-loading']= 'aan het laden';
-$messages['categorytree-nothing-found']= 'Niets gevonden, sorry.';
+$messages['categorytree-nothing-found']= 'Deze categorie heeft geen subcategorieën.';
 $messages['categorytree-no-subcategories']= 'Geen subcategorieën.';
 $messages['categorytree-no-pages']= 'Geen artikelen of subcategorieën.';
 $messages['categorytree-not-found']= "Categorie ''\$1'' niet gevonden";
+
+$messages['categorytree-show-list'] = "Toon als lijst";
+$messages['categorytree-show-tree'] = "Toon als boomstructuur";
+$messages['categorytree-too-many-subcats'] = "Kan de subcategorieën niet als boomstructuur tonen, er zijn er te veel.";
+
 ?>
