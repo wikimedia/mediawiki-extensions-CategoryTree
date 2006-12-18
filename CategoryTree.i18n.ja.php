aaf84786076b2f5a06affff1e969496044db5f86
Rotem Liss <rotem@users.mediawiki.org>
2006-12-18 10:42:53 +0000

    (bug 8285) Update to Japanese localisation (ja) in extensions

---
+++ CategoryTree.i18n.ja.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
+
+$messages['categorytree']= 'カテゴリツリー';
+$messages['categorytree-tab']= 'ツリー';
+$messages['categorytree-header']= 
+'カテゴリの中身をツリー構造としてを見るために、そのカテゴリ名を入力してください。この機能は、Ajaxとして知られているJavaScriptを使用していることに注意してください。もしあなたが使っているブラウザが非常に古かったり、JavaScriptを有効にしていないのであれば、動作しません。
+';
+
+$messages['categorytree-category']= 'カテゴリ名: ';
+$messages['categorytree-go']= 'ツリーを見る';
+$messages['categorytree-parents']= '上位カテゴリ';
+
+$messages['categorytree-mode-categories']= 'カテゴリのみ';
+$messages['categorytree-mode-pages']= '画像以外の全ページ';
+$messages['categorytree-mode-all']= 'すべてのページ';
+
+$messages['categorytree-collapse']= '下位カテゴリを非表示';
+$messages['categorytree-expand']= '下位カテゴリを再表示';
+$messages['categorytree-load']= '下位カテゴリを表示';
+$messages['categorytree-loading']= '読み込み中';
+$messages['categorytree-nothing-found']= '存在しません';
+$messages['categorytree-no-subcategories']= 'サブカテゴリはありません';
+$messages['categorytree-no-pages']= 'ページやサブカテゴリはありません';
+$messages['categorytree-not-found']= "カテゴリ \" <i>$1</i> \" はありません";
+
+$messages['categorytree-show-list'] = "一覧で表示";
+$messages['categorytree-show-tree'] = "ツリー形式で表示";
+$messages['categorytree-too-many-subcats'] = "サブカテゴリの数が多すぎるため、ツリー表示できません。";
+
+?>
