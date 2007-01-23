bda6c25cae46bf78cc483b11a29ba68fc35e009a
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-01-23 17:18:36 +0000

    * Slovak translations to extensions

--- CategoryTree.i18n.sk.php
+++ CategoryTree.i18n.sk.php
@@ -30,7 +30,7 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
   */
   
-$messages['categorytree']= 'StromKategórií';
+$messages['categorytree']= 'Strom kategórií';
 $messages['categorytree-tab']= 'Strom';
 $messages['categorytree-header']= 
 'Zadajte názov kategórie, ktorej obsah sa má zobraziť ako stromová štruktúra.
