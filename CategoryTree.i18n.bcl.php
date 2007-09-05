9e5d12092cf8ac076f7156225ddceb144975277a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 21:38:14 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * bcl: 15 extensions updated

---
+++ CategoryTree.i18n.bcl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author editors of wikipedia
+ * @copyright  2007 Filipinayzd
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree-category' => 'Kategorya',
+	'categorytree-mode-all' => 'gabos na mga pahina',
+	'categorytree-loading' => 'pigkakarga',
+	'categorytree-nothing-found' => 'mayong nahanap',
+	'categorytree-no-subcategories' => 'mayong mga sub-kategorya',
+	'categorytree-no-pages' => 'mayong mga pahina o sub-kategorya',
+	'categorytree-retry' => 'Paki halat mûna tapos probaran giraray.',
+	'categorytree-show-list' => 'Ipahiling an lista',
+);
