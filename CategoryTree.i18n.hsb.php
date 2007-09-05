c87e33a0ee90e3566440ab150498be64d50120b0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 21:49:57 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * hsb: 8 extensions updated

---
+++ CategoryTree.i18n.hsb.php
@@ -0,0 +1,56 @@
+<?php
+/*
+  Internationalisation file for the CategoryTree extension
+  Copyright (C) 2006, Daniel Kinzler, brightbyte.de;
+  Hornjoserbsce translation by Michawiki and Dundak.
+
+  This file is licensed under the GPL and the GFDL:
+
+
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+
+$messages = array(
+	'categorytree' => 'Kategorijowy štom',
+	'categorytree-tab' => 'Štom',
+	'categorytree-header' => 'Zapisaj mjeno kategorije, zo by jeje wobsah jako štomowu strukturu widźał. Wobkedźbuj, zo su za to wěste JavaScriptowe funkcije (AJAX) trjeba. Jeli maš jara stary wobhladowak abo jeli JavaScript je wupinjeny, to snano njebudźe fungować.',
+	'categorytree-category' => 'Kategorija',
+	'categorytree-go' => 'Štom pokazać',
+	'categorytree-parents' => 'Nadkategorije',
+	'categorytree-mode-categories' => 'jenož Kategorije',
+	'categorytree-mode-pages' => 'strony nimo wobrazow',
+	'categorytree-mode-all' => 'wšě strony',
+	'categorytree-collapse' => 'schować',
+	'categorytree-expand' => 'pokazać',
+	'categorytree-load' => 'začitać',
+	'categorytree-loading' => 'čita so ...',
+	'categorytree-nothing-found' => 'ničo namakane',
+	'categorytree-no-subcategories' => 'žane podkategorije',
+	'categorytree-no-pages' => 'žane strony abo podkategorije',
+	'categorytree-not-found' => 'Kategorija \'\'$1\'\' njenamakana',
+	'categorytree-error' => 'Problem při čitanju datow.',
+	'categorytree-retry' => 'Prošu čakaj wokomik a spytaj potom hišće raz.',
+	'categorytree-show-list' => 'Jako lisćinu zwobraznić',
+	'categorytree-show-tree' => 'Jako štom zwobraznić',
+	'categorytree-too-many-subcats' => 'Njemóžno podkategorije jako štom zwobraznić, je přewjele z nich',
+);
