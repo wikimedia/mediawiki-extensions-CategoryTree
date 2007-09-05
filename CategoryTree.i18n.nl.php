7c4e56e20e7a6a4d807aa8786c4de223831514a5
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 22:01:11 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * nl: extension CategoryTree updated

--- CategoryTree.i18n.nl.php
+++ CategoryTree.i18n.nl.php
@@ -9,32 +9,29 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
-$messages['categorytree']= 'Categorieboom';
-$messages['categorytree-tab']= 'Boom';
-$messages['categorytree-header']=
-'Geef een categorienaam in om de inhoud als een boomstructuur te bekijken.
+$messages = array(
+	'categorytree' => 'Categorieboom',
+	'categorytree-tab' => 'Boom',
+	'categorytree-header' => 'Geef een categorienaam in om de inhoud als een boomstructuur te bekijken.
 Let op: deze functie gebruikt JavaScript-functionaliteit die bekend staat als AJAX.
-Als u een erg verouderde browser heeft of JavaScript staat uitgeschakeld, dan werkt deze functie niet.
-';
-
-$messages['categorytree-category']= 'Categorie';
-$messages['categorytree-go']= 'Laden';
-$messages['categorytree-parents']= 'Bovenliggende categorieën';
-
-$messages['categorytree-mode-categories']= 'alleen categorieën';
-$messages['categorytree-mode-pages']= 'geen afbeeldingen';
-$messages['categorytree-mode-all']= 'alle pagina\'s';
-
-$messages['categorytree-collapse']= 'inklappen';
-$messages['categorytree-expand']= 'uitklappen';
-$messages['categorytree-load']= 'laden';
-$messages['categorytree-loading']= 'aan het laden';
-$messages['categorytree-nothing-found']= 'Deze categorie heeft geen subcategorieën.';
-$messages['categorytree-no-subcategories']= 'Geen subcategorieën.';
-$messages['categorytree-no-pages']= 'Geen artikelen of subcategorieën.';
-$messages['categorytree-not-found']= "Categorie ''\$1'' niet gevonden";
-
-$messages['categorytree-show-list'] = "Toon als lijst";
-$messages['categorytree-show-tree'] = "Toon als boomstructuur";
-$messages['categorytree-too-many-subcats'] = "Kan de subcategorieën niet als boomstructuur tonen, er zijn er te veel.";
-
+Als u een erg verouderde browser heeft of JavaScript staat uitgeschakeld, dan werkt deze functie niet.',
+	'categorytree-category' => 'Categorie',
+	'categorytree-go' => 'Laden',
+	'categorytree-parents' => 'Bovenliggende categorieën',
+	'categorytree-mode-categories' => 'alleen categorieën',
+	'categorytree-mode-pages' => 'geen afbeeldingen',
+	'categorytree-mode-all' => 'alle pagina\'s',
+	'categorytree-collapse' => 'inklappen',
+	'categorytree-expand' => 'uitklappen',
+	'categorytree-load' => 'laden',
+	'categorytree-loading' => 'aan het laden',
+	'categorytree-nothing-found' => 'Deze categorie heeft geen subcategorieën.',
+	'categorytree-no-subcategories' => 'Geen subcategorieën.',
+	'categorytree-no-pages' => 'Geen artikelen of subcategorieën.',
+	'categorytree-not-found' => 'Categorie \'\'$1\'\' niet gevonden',
+	'categorytree-error' => 'Probleem bij het laden van de gegevens.',
+	'categorytree-retry' => 'Wacht alstublieft even en probeer het dan opnieuw.',
+	'categorytree-show-list' => 'Toon als lijst',
+	'categorytree-show-tree' => 'Toon als boomstructuur',
+	'categorytree-too-many-subcats' => 'Kan de subcategorieën niet als boomstructuur tonen, er zijn er te veel.',
+);
