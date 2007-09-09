32a9ebf35de55cfbca4d2cd465dd092473f801cd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-09 18:47:23 +0000

    Localisation updates from Betawiki. * Fixes and additions to 6 extensions for ar.

---
+++ CategoryTree.i18n.ar.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree' => 'شجرة تصنيف',
+	'categorytree-tab' => 'شجرة',
+	'categorytree-header' => 'أدخل اسم تصنيف لترى محتوياته كتركيب شجري.
+لاحظ أن هذا يتطلب خاصية جافاسكريبت متقدمة معروفة كأجاكس.
+لو كنت تمتلك متصفحا قديما جدا، أو لديك الجافاسكريبت معطلة، فلن تعمل.',
+	'categorytree-category' => 'تصنيف',
+	'categorytree-go' => 'عرض الشجرة',
+	'categorytree-parents' => 'الآباء',
+	'categorytree-mode-categories' => 'تصنيفات فقط',
+	'categorytree-mode-pages' => 'الصفحات ماعدا الصور',
+	'categorytree-mode-all' => 'كل الصفحات',
+	'categorytree-collapse' => 'ضغط',
+	'categorytree-expand' => 'فرد',
+	'categorytree-load' => 'تحميل',
+	'categorytree-loading' => 'جاري التحميل',
+	'categorytree-nothing-found' => 'لم يتم العثور على شيء',
+	'categorytree-no-subcategories' => 'لا تصنيفات فرعية',
+	'categorytree-no-pages' => 'لا صفحات و لا تصنيفات فرعية',
+	'categorytree-not-found' => 'التصنيف <i>$1</i> لم يتم العثور عليه',
+	'categorytree-error' => 'مشكلة في تحميل البيانات.',
+	'categorytree-retry' => 'من فضلك انتظر لحظة و حاول مرة أخرى.',
+	'categorytree-show-list' => 'اعرض كقائمة',
+	'categorytree-show-tree' => 'اعرض كشجرة',
+	'categorytree-too-many-subcats' => 'لا يمكن عرض التصنيفات الفرعية كشجرة، يوجد الكثير منهم.',
+);
