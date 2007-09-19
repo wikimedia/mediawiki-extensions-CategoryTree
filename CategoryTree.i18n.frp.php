a69912f30d5ce2e3677240072acdae3885606284
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-19 14:30:16 +0000

    Localisation updates from Betawiki. * Fixes and additions to 27 extensions for bg, br, csb, da, eo, et, frp, gl, hak, hsb.

---
+++ CategoryTree.i18n.frp.php
@@ -0,0 +1,58 @@
+<?php
+/*
+  Internationalisation file for the CategoryTree extension
+  Copyright (C) 2006, Daniel Kinzler, brightbyte.de
+  Translation by ChrisPtDe
+ 
+  This file is licensed under the GPL and the GFDL:
+  
+  
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+  
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+  
+$messages = array(
+	'categorytree' => 'Arborèscence de les catègories',
+	'categorytree-tab' => 'Âbro',
+	'categorytree-header' => 'Entrâd un nom de catègorie por vêre son contegnu en structura arborèscenta.
+Volyéd notar que cen utilise des fonccionalitâts JavaScript avanciêes cognues desot lo nom d’AJAX.
+Se vos avéd un rudo viely navigator ou que vos éd pas activâ lo JavaScript, cen fonccionerat pas.',
+	'categorytree-category' => 'Catègorie',
+	'categorytree-go' => 'Afichiér l’arborèscence',
+	'categorytree-parents' => 'Sur-catègorie(s) ',
+	'categorytree-mode-categories' => 'ren que les catègories',
+	'categorytree-mode-pages' => 'pâges sen les émâges',
+	'categorytree-mode-all' => 'totes les pâges',
+	'categorytree-collapse' => 'Recllôre',
+	'categorytree-expand' => 'Dèvelopar',
+	'categorytree-load' => 'Uvrir',
+	'categorytree-loading' => 'uvèrtura...',
+	'categorytree-nothing-found' => 'Pas trovâ, dèsolâ.',
+	'categorytree-no-subcategories' => 'Gins de sot-catègorie.',
+	'categorytree-no-pages' => 'Gins d’articllo ou de sot-catègorie.',
+	'categorytree-not-found' => 'La catègorie <tt>$1</tt> at pas étâ trovâ.',
+	'categorytree-error' => 'Problèmo de chargement de les balyês.',
+	'categorytree-retry' => 'Atendéd un moment et pués tornâd èprovar.',
+	'categorytree-show-list' => 'Afichiér en lista',
+	'categorytree-show-tree' => 'Afichiér en arborèscence',
+	'categorytree-too-many-subcats' => 'Les sot-catègories sont trop nombroses por étre afichiêes desot fôrma d’âbro.',
+);
