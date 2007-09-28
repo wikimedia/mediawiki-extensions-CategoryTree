29ef7f647c6cf42bb85ab5709c5c288fc56f99c5
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-28 08:59:16 +0000

    Localisation updates from Betawiki. * Fixes and additions to 26 extensions for am, cs, ext, fo, hsb, la.

---
+++ CategoryTree.i18n.am.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree' => 'የመደቦች ዛፍ',
+	'categorytree-header' => '[+] ተጭነው ንዑሱ-መደብ ይዘረጋል፣ [-] ተጭነው ደግሞ ይመልሳል። 
+
+በግራ በኩል ባለው ሳጥን ውስጥ የመደቡን ስም ዝም ብለው መጻፍ ይችላሉ። (የዚሁ ዊኪ መደብ ስሞች ለመመልከት፣ [[Special:Mostlinkedcategories|እዚህ ይጫኑ]]።) ከዚያ፥ ምን ያሕል ንዑስ-መደቦች እንዳሉበት ለማየት «ዛፉ ይታይ» የሚለውን ይጫኑ። በቀኝ በኩል ካለው ሳጥን \'all pages\' ከመረጡ፥ በየመደቡ ውስጥ ያሉት መጣጥፎች በተጨማሪ ይታያሉ።
+
+\'\'(ማስታወሻ: ይህ በኮምፒውተርዎ እንዲሠራ \'ጃቫ\' የሚችል ዌብ-ብራውዘር ያስፈልጋል።)\'\'',
+	'categorytree-category' => 'የመደብ ስም፦',
+	'categorytree-go' => 'ዛፉ ይታይ',
+	'categorytree-parents' => 'ላዕላይ መደቦች',
+	'categorytree-loading' => 'ሊመጣ ነው',
+	'categorytree-not-found' => '«$1» የተባለ መደብ የለም።',
+);
