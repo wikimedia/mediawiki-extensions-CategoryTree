fecf1499e3088019a1c55a1ff702f4d485adb953
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-30 12:58:32 +0000

    Localisation updates from Betawiki. * Fixes and additions to 38 extensions for am, ar, hu, nds, nl.

---
+++ CategoryTree.i18n.nds.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  Internationalisation file for the CategoryTree extension.
+  (C) 2007 - Plattdüütsch translation by Slomox
+
+  This file is licensed under the GPL and the GFDL:
+
+
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+
+$messages = array(
+	'categorytree' => 'Kategorie-Boom',
+	'categorytree-tab' => 'Boom',
+	'categorytree-header' => 'Kategorienaam ingeven, den Inholt as Boomstruktur to sehn. Schasst di bewusst wesen, dat Javascript un de AJAX-Funkschoon dor för bruukt warrt. Wenn dien Nettkieker to oolt is oder du keen Javascript hest, denn warrt dat nix.',
+	'categorytree-category' => 'Kategorie',
+	'categorytree-go' => 'Boom wiesen',
+	'categorytree-parents' => 'Öllernkategorien',
+	'categorytree-mode-categories' => 'blot Kategorien',
+	'categorytree-mode-pages' => 'Sieden ahn Biller',
+	'categorytree-mode-all' => 'all Sieden',
+	'categorytree-collapse' => 'nich ganz wiesen',
+	'categorytree-expand' => 'ganz wiesen',
+	'categorytree-load' => 'laden',
+	'categorytree-loading' => 'läädt',
+	'categorytree-nothing-found' => 'nix funnen',
+	'categorytree-no-subcategories' => 'kene Ünnerkategorien',
+	'categorytree-no-pages' => 'kene Sieden oder Ünnerkategorien',
+	'categorytree-not-found' => 'Kategorie <i>$1</i> nich funnen',
+	'categorytree-error' => 'Problem bi’t Laden vun de Daten',
+	'categorytree-retry' => 'Tööv en beten un denn versöök dat noch wedder.',
+	'categorytree-show-list' => 'as List wiesen',
+	'categorytree-show-tree' => 'as Boom wiesen',
+	'categorytree-too-many-subcats' => 'Kann Ünnerkategorien nich as Boom wiesen, sünd to veel.',
+);
