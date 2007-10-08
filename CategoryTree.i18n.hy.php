2a6e4859b3ad8f91ca61f702fb70d4c334450496
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-10-08 07:11:40 +0000

    Localisation updates from Betawiki. * Fixes and additions to 7 extensions for hy (Armenian) * Core messages update for hy

---
+++ CategoryTree.i18n.hy.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree' => 'Կատեգորիաների ծառ',
+	'categorytree-tab' => 'Ծառ',
+	'categorytree-header' => 'Մուտքագրեք կատեգորիայի անունը` ծառի համակարգը տեսնելու համար։
+Ի նկատի ունեցեք, որ սա հնարավոր է միայն ձեր բրաուզերի կողմից AJAX-ի ֆունկցիանալության դեպքում։
+Եթե դուք աշխատում եք շատ հին բրաուզերով, կամ ձեր JavaScript-ը անջատված է` այն չի գործի։',
+	'categorytree-category' => 'Կատեգորիա',
+	'categorytree-go' => 'Ցույց տալ ծառը',
+	'categorytree-parents' => 'Ծնողներ',
+	'categorytree-mode-categories' => 'միայն կատեգորիաները',
+	'categorytree-mode-pages' => 'բացի պատկերներից',
+	'categorytree-mode-all' => 'բոլոր էջերը',
+	'categorytree-collapse' => 'փակել',
+	'categorytree-expand' => 'բացել',
+	'categorytree-load' => 'բեռնել',
+	'categorytree-loading' => 'բեռնվում է',
+	'categorytree-nothing-found' => 'ենթակատեգորիան չկան',
+	'categorytree-no-subcategories' => 'ենթակատեգորիաներ չկան',
+	'categorytree-no-pages' => 'ենթակատեգորիան և էջեր չկան',
+	'categorytree-not-found' => '«<i>$1</i>» կատեգորիան չի գտնվել',
+	'categorytree-error' => 'Տվյալների բեռնումը չհաջողվեց',
+	'categorytree-retry' => 'Խնդրում ենք սպասել մեկ ակնթարթ և փորձել կրկին։',
+	'categorytree-show-list' => 'Ցանկի տեսքով',
+	'categorytree-show-tree' => 'Ծառի տեսքով',
+	'categorytree-too-many-subcats' => 'Հնարավոր չէ ցուցադրել ենթակատեգորիաները ծառի տեսքով՝ չափից շատ են։',
+);
