d5fbafff9e00c436ee48d51cc7c350ecf42ae6fd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-10-29 23:28:45 +0000

    Localisation updates from Betawiki. * Fixes and additions to 41 extensions for gl, hr, ja, pl, pms

--- CategoryTree.i18n.ja.php
+++ CategoryTree.i18n.ja.php
@@ -6,31 +6,26 @@
  * @addtogroup Extensions
  */
 
-$messages['categorytree']= 'カテゴリツリー';
-$messages['categorytree-tab']= 'ツリー';
-$messages['categorytree-header']= 
-'カテゴリの中身をツリー構造としてを見るために、そのカテゴリ名を入力してください。この機能は、Ajaxとして知られているJavaScriptを使用していることに注意してください。もしあなたが使っているブラウザが非常に古かったり、JavaScriptを有効にしていないのであれば、動作しません。
-';
-
-$messages['categorytree-category']= 'カテゴリ名: ';
-$messages['categorytree-go']= 'ツリーを見る';
-$messages['categorytree-parents']= '上位カテゴリ';
-
-$messages['categorytree-mode-categories']= 'カテゴリのみ';
-$messages['categorytree-mode-pages']= '画像以外の全ページ';
-$messages['categorytree-mode-all']= 'すべてのページ';
-
-$messages['categorytree-collapse']= '下位カテゴリを非表示';
-$messages['categorytree-expand']= '下位カテゴリを再表示';
-$messages['categorytree-load']= '下位カテゴリを表示';
-$messages['categorytree-loading']= '読み込み中';
-$messages['categorytree-nothing-found']= '存在しません';
-$messages['categorytree-no-subcategories']= 'サブカテゴリはありません';
-$messages['categorytree-no-pages']= 'ページやサブカテゴリはありません';
-$messages['categorytree-not-found']= "カテゴリ \" <i>$1</i> \" はありません";
-
-$messages['categorytree-show-list'] = "一覧で表示";
-$messages['categorytree-show-tree'] = "ツリー形式で表示";
-$messages['categorytree-too-many-subcats'] = "サブカテゴリの数が多すぎるため、ツリー表示できません。";
-
-
+$messages = array(
+	'categorytree' => 'カテゴリツリー',
+	'categorytree-tab' => 'ツリー',
+	'categorytree-header' => 'カテゴリの中身をツリー構造としてを見るために、そのカテゴリ名を入力してください。この機能は、Ajaxとして知られているJavaScriptを使用していることに注意してください。もしあなたが使っているブラウザが非常に古かったり、JavaScriptを有効にしていないのであれば、動作しません。',
+	'categorytree-category' => 'カテゴリ名: ',
+	'categorytree-go' => 'ツリーを見る',
+	'categorytree-parents' => '上位カテゴリ',
+	'categorytree-mode-categories' => 'カテゴリのみ',
+	'categorytree-mode-pages' => '画像以外の全ページ',
+	'categorytree-mode-all' => 'すべてのページ',
+	'categorytree-collapse' => '下位カテゴリを非表示',
+	'categorytree-expand' => '下位カテゴリを再表示',
+	'categorytree-load' => '下位カテゴリを表示',
+	'categorytree-loading' => '読み込み中',
+	'categorytree-nothing-found' => '存在しません',
+	'categorytree-no-subcategories' => 'サブカテゴリはありません',
+	'categorytree-no-pages' => 'ページやサブカテゴリはありません',
+	'categorytree-not-found' => 'カテゴリ " <i>$1</i> " はありません',
+	'categorytree-retry' => '暫く経った後に再度試してください。',
+	'categorytree-show-list' => '一覧で表示',
+	'categorytree-show-tree' => 'ツリー形式で表示',
+	'categorytree-too-many-subcats' => 'サブカテゴリの数が多すぎるため、ツリー表示できません。',
+);
