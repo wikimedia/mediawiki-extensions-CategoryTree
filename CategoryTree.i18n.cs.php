70e0dc3898d6f05137201fd6b57db867bf316e15
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-14 23:13:09 +0000

    Localisation updates from Betawiki. * Fixes and additions to 5 extensions for cs, el, fr

--- CategoryTree.i18n.cs.php
+++ CategoryTree.i18n.cs.php
@@ -33,15 +33,14 @@
 $messages = array(
 	'categorytree' => 'Strom kategorií',
 	'categorytree-tab' => 'Strom',
-	'categorytree-header' => 'Zadejte název kategorie k zobrazení ve stromové struktuře.
-Tato funkce vyžaduje pokročilé funkce JavaScriptu známé jako AJAX.
-Jestliže máte velmi starý prohlížeč nebo vypnutý JavaScript, nezobrazí se strom správně nebo vůbec.
-',
+	'categorytree-header' => 'Zadejte název kategorie k&nbsp;zobrazení jejího obsahu jako stromové struktury.
+
+(Tato funkce vyžaduje pokročilé funkce JavaScriptu známé jako Ajax. Jestliže máte velmi starý prohlížeč nebo vypnutý JavaScript, nezobrazí se strom správně nebo vůbec.)',
 	'categorytree-category' => 'Kategorie',
 	'categorytree-go' => 'Zobrazit',
 	'categorytree-parents' => 'Nadřazené kategorie',
-	'categorytree-mode-categories' => 'ukaž pouze kategorie',
-	'categorytree-mode-pages' => 'stránky kromě obrázků',
+	'categorytree-mode-categories' => 'pouze kategorie',
+	'categorytree-mode-pages' => 'stránky kromě souborů',
 	'categorytree-mode-all' => 'všechny stránky',
 	'categorytree-collapse' => 'zavřít',
 	'categorytree-expand' => 'otevřít',
@@ -50,10 +49,10 @@ Jestliže máte velmi starý prohlížeč nebo vypnutý JavaScript, nezobrazí s
 	'categorytree-nothing-found' => 'nic nebylo nalezeno',
 	'categorytree-no-subcategories' => 'žádné podkategorie.',
 	'categorytree-no-pages' => 'žádné články ani podkategorie.',
-	'categorytree-not-found' => 'Kategorie <i>$1</i> nenalezena',
+	'categorytree-not-found' => 'Kategorie <em>$1</em> nenalezena',
 	'categorytree-error' => 'Chyba při načítání dat.',
 	'categorytree-retry' => 'Počkejte chvilku a zkuste to znova.',
 	'categorytree-show-list' => 'Zobrazi jako seznam',
 	'categorytree-show-tree' => 'Zobrazit jako strom',
-	'categorytree-too-many-subcats' => 'Podkategorie není možné zobrazit ve stromové struktuře, protože je jich příliš mnoho..',
+	'categorytree-too-many-subcats' => 'Podkategorie není možné zobrazit ve stromové struktuře, protože je jich příliš mnoho.',
 );
