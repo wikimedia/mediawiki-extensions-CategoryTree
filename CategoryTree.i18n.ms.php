508d0d3b8216bb61a9f887ef728569361ef9f4d9
Raimond Spekking <raymond@users.mediawiki.org>
2007-11-19 12:17:56 +0000

    * (bug 12018) Add Malay translation   Patch by Aviator

---
+++ CategoryTree.i18n.ms.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @addtogroup Extensions
+ * @author Copyright (C) 2006, Daniel Kinzler, brightbyte.de
+ * @author Copyright (C) 2007, Aviator, http://ms.wikipedia.org/wiki/Pengguna:Aviator
+ *
+ * Licensed under the GNU General Public Licence, http://www.gnu.org/licenses/gpl.html
+ */
+
+$messages['categorytree']= 'SalasilahKategori';
+$messages['categorytree-tab']= 'Salasilah';
+$messages['categorytree-header']= 
+'Masukkan suatu nama kategori untuk melihat kandungannya dalam bentuk struktur salasilah. 
+Ciri ini memerlukan kebolehan JavaScript maju yang dikenali sebagai AJAX.
+Sekiranya anda menggunakan pelayar web yang lama, atau mematikan JavaScript, ciri ini tidak akan menjadi.
+';
+
+$messages['categorytree-category']= 'Kategori';
+$messages['categorytree-go']= 'Tunjukkan Salasilah';
+$messages['categorytree-parents']= 'Induk';
+
+$messages['categorytree-mode-categories']= 'kategori sahaja';
+$messages['categorytree-mode-pages']= 'laman kecuali imej';
+$messages['categorytree-mode-all']= 'semua laman';
+
+$messages['categorytree-collapse']= 'tutup';
+$messages['categorytree-expand']= 'buka';
+$messages['categorytree-load']= 'muat';
+$messages['categorytree-loading']= 'memuat';
+$messages['categorytree-nothing-found']= 'kosong';
+$messages['categorytree-no-subcategories']= 'tiada subkategori';
+$messages['categorytree-no-pages']= 'tiada laman atau subkategori';
+$messages['categorytree-not-found']= "Kategori <i>$1</i> tiada";
+$messages['categorytree-error'] = 'Berlaku masalah ketika memuat data.';
+$messages['categorytree-retry'] = 'Sila tunggu sebentar dan cuba lagi.';
+
+$messages['categorytree-show-list'] = "Tunjuk dalam bentuk senarai";
+$messages['categorytree-show-tree'] = "Tunjuk dalam bentuk salasilah";
+$messages['categorytree-too-many-subcats'] = "Terlalu banyak subkategori, tidak boleh menunjukkannya dalam bentuk salasilah.";
