8ee14597ac6952595ad9d555b957a0d02afefc7f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-23 10:00:57 +0000

    Localisation updates from Betawiki. * Fixes and additions to 31 extensions for ar, el, fr, gl, hr, hsb, ie, is, kn, nl, stq

--- CategoryTree.i18n.nl.php
+++ CategoryTree.i18n.nl.php
@@ -4,8 +4,8 @@
  * Internationalisation file for the CategoryTree extension
  *
  * @addtogroup Extensions
- * @author Dutch translation by JePe and Siebrand, source file by Daniel Kinzler, brightbyte.de
- * @copyright © 2006 Daniel Kinzler, JePe, Siebrand
+ * @author Dutch translation by JePe, Siebrand, and Betawiki users. Source file by Daniel Kinzler, brightbyte.de
+ * @copyright © 2006-2007 Daniel Kinzler, JePe, Siebrand, Betawik users
  * @licence GNU General Public Licence 2.0 or later
  */
 
@@ -27,7 +27,7 @@ Als u een erg verouderde browser heeft of JavaScript staat uitgeschakeld, dan we
 	'categorytree-loading' => 'aan het laden',
 	'categorytree-nothing-found' => 'Deze categorie heeft geen ondercategorieën.',
 	'categorytree-no-subcategories' => 'Geen ondercategorieën.',
-	'categorytree-no-pages' => 'Geen artikelen of ondercategorieën.',
+	'categorytree-no-pages' => 'Geen pagina\'s of ondercategorieën.',
 	'categorytree-not-found' => 'Categorie \'\'$1\'\' niet gevonden',
 	'categorytree-error' => 'Probleem bij het laden van de gegevens.',
 	'categorytree-retry' => 'Wacht alstublieft even en probeer het dan opnieuw.',
