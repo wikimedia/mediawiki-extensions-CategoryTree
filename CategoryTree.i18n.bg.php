28fa021d2cd7e6af15b5deea7c3d3a4b1f638231
Raimond Spekking <raymond@users.mediawiki.org>
2007-11-26 11:27:47 +0000

    * (bug 12099) Add Bulgarian translation   Patch by DCLXVI

---
+++ CategoryTree.i18n.bg.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @addtogroup Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2006 Daniel Kinzler
+ * @Bulgarian translation © 2007 Dimitar Tsonev (Gregg), crossword.crozzword.com
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['categorytree']= 'Дърво на категориите';
+$messages['categorytree-tab']= 'Дърво';
+$messages['categorytree-header']= 'Въведете категория, за да видите съдържанието й в дървовиден вид от категории. Имайте предвид, че това изисква допълнителна JavaScript-функционалност, позната като AJAX. Тази възможност не може да бъде използвана ако използвате стар браузър или сте изключили поддържането на JavaScript.';
+
+$messages['categorytree-category']= 'Категория';
+$messages['categorytree-go']= 'Показване';
+$messages['categorytree-parents']= 'Родителски категории';
+
+$messages['categorytree-mode-categories']= 'само категории';
+$messages['categorytree-mode-pages']= 'страници, без файлове';
+$messages['categorytree-mode-all']= 'всички страници';
+
+$messages['categorytree-collapse']= 'събиране';
+$messages['categorytree-expand']= 'разпъване';
+$messages['categorytree-load']= 'зареждане';
+$messages['categorytree-loading']= 'зареждане';
+$messages['categorytree-nothing-found']= 'няма открити поддиректории';
+$messages['categorytree-no-subcategories']= 'няма подкатегории';
+$messages['categorytree-no-pages']= 'няма страници или подкатегории';
+$messages['categorytree-not-found']= "Не беше намерена категория <i>$1</i>";
+$messages['categorytree-error'] = 'Възникна проблем при зареждане на информацията.';
+$messages['categorytree-retry'] = 'Изчакайте малко и опитайте отново.';
+
+$messages['categorytree-show-list'] = "Показване като списък";
+$messages['categorytree-show-tree'] = "Показване като дърво";
+$messages['categorytree-too-many-subcats'] = "Съществуват твърде много подкатегории, невъзможно е да бъдат показани като дърво.";
