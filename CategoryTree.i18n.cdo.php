60ee40d0d39d6d00f0c85d01377db212f0a8824e
Raimond Spekking <raymond@users.mediawiki.org>
2007-11-26 11:56:55 +0000

    * (Bug 12102) Add Min Dong localisation and its extension messages

---
+++ CategoryTree.i18n.cdo.php
@@ -0,0 +1,47 @@
+<?php
+/*
+  Internationalisation file for the CategoryTree extension
+  Copyright (C) 2006, Daniel Kinzler, brightbyte.de
+  Min Dong by GnuDoyng
+ 
+  This file is licensed under the GPL and the GFDL:
+  
+  
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+  
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+
+$messages['categorytree']= 'Lôi-biék chéu';
+$messages['categorytree-tab']= 'Chéu';
+$messages['categorytree-header']= 'Sṳ̆-ĭk lôi-biék miàng-chĭng, káng ĭ gì chéu-hìng giék-gáiu. Chiāng cé̤ṳ-é, ciā hiĕk-miêng sāi-ê̤ṳng siŏh cṳ̄ng gŏ̤-gék JavaScript gé-sŭk, giéu lō̤ AJAX. Nṳ̄ nâ sāi-ê̤ṳng guó-sì gì báuk-lāng-ké, hĕ̤k-ciā cĕk lâi JavaScript, cêu mâ̤ ciáng-siòng gĕ̤ng-cáuk.';
+
+$messages['categorytree-category']= 'Hŭng-lôi';
+$messages['categorytree-go']= 'Hiēng-sê chéu';
+
+$messages['categorytree-mode-categories']= 'nâ ô lôi-biék';
+$messages['categorytree-mode-pages']= 'dù-piéng ī-nguôi gì hiĕk-miêng';
+$messages['categorytree-mode-all']= 'tĕ̤k-chṳ̄';
+
+$messages['categorytree-loading']= 'tĕ̤k-chṳ̄';
+$messages['categorytree-no-subcategories']= 'mò̤ cṳ̄-lôi-biék';
+$messages['categorytree-no-pages']= 'mò̤ hiĕk-miêng hĕ̤k cṳ̄-lôi-biék';
+$messages['categorytree-not-found']= "Mò̤ tō̤ diŏh lôi-biék <i>$1</i>";
