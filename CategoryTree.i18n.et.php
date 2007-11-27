bc674f264dc24724216e8aa6dc7931fe60d389a1
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-27 09:49:24 +0000

    Localisation updates from Betawiki. * Fixes and additions to 50 extensions for ar, el, et, fr, hr, hsb, io, ka, la, nap, nl, pt, sk, stq, tet, vo

---
+++ CategoryTree.i18n.et.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree' => 'Kategooriapuu',
+	'categorytree-category' => 'Kategooria',
+	'categorytree-mode-categories' => 'ainult kategooriad',
+	'categorytree-mode-pages' => 'leheküljed, välja arvatud pildid',
+	'categorytree-no-subcategories' => 'alamkategooriaid ei ole',
+	'categorytree-show-list' => 'Näita nimekirjana',
+	'categorytree-show-tree' => 'Näita kategooriapuuna',
+);
