8a8b4325b4d1ac757f354d388cfadc98ec25a45e
Shinjiman <shinjiman@users.mediawiki.org>
2007-12-02 14:49:27 +0000

    * Wu translations   Provided by Wtzdj

---
+++ CategoryTree.i18n.wuu.php
@@ -0,0 +1,58 @@
+<?php
+/*
+  Internationalisation file for the CategoryTree extension
+  Copyright (C) 2006, Daniel Kinzler, brightbyte.de
+  Wu by Wtzdj
+ 
+  This file is licensed under the GPL and the GFDL:
+  
+  
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+  
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+  
+$messages['categorytree']= '分类树';
+$messages['categorytree-tab']= '树形';
+#$messages['categorytree-header']= 
+'';
+  
+$messages['categorytree-category']= '分类';
+$messages['categorytree-go']= '显示树形';
+#$messages['categorytree-parents']= '';
+  
+$messages['categorytree-mode-categories']= '仅分类';
+$messages['categorytree-mode-pages']= '除脱图片以外个页面';
+$messages['categorytree-mode-all']= '所有页面';
+  
+$messages['categorytree-collapse']= '抈出来';
+$messages['categorytree-expand']= '放开来';
+#$messages['categorytree-load']= '';
+#$messages['categorytree-loading']= '';
+$messages['categorytree-nothing-found']= '一样也朆寻着';
+$messages['categorytree-no-subcategories']= '呒拨子分类';
+$messages['categorytree-no-pages']= '呒拨页面或者子分类';
+#$messages['categorytree-not-found']= "";
+
+$messages['categorytree-show-list'] = "显示成功列表";
+$messages['categorytree-show-tree'] = "显示成功树";
+$messages['categorytree-too-many-subcats'] = "子分类忒多哉，显示弗过来哉。";
+
