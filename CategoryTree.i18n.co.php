44d46e98aa383d6655248ae5b9ec8c118bfd45ce
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-08 12:45:20 +0000

    Localisation updates from Betawiki. * Fixes and additions to 56 extensions for an, bg, co, el, fi, hsb, nl, ro

---
+++ CategoryTree.i18n.co.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree-category' => 'Categuria',
+	'categorytree-mode-pages' => 'pagine senza imagin',
+	'categorytree-mode-all' => 'tutte e pagine',
+);
