5754dd20fcbafc4dd4afe5695492d2646f13e5a0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-11 22:38:12 +0000

    Localisation updates from Betawiki. * Fixes and additions to 47 extensions for ar, bg, cs, el, fr, gl, ja, la, loz, no, stq

---
+++ CategoryTree.i18n.stq.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+$messages = array(
+	'categorytree' => 'Kategorieboom',
+	'categorytree-tab' => 'Boom',
+	'categorytree-header' => 'Wiest foar ju anroate Kategorie do Unnerkategorien in n Boomstruktuur.
+Disse Siede bruukt bestimde JavaScript-Funktione (AJAX).
+In gjucht oolde Browsere, of wan Javascript ouschalted is, funktioniert disse Siede eventuell nit.',
+	'categorytree-category' => 'Kategorie',
+	'categorytree-go' => 'Leede',
+	'categorytree-parents' => 'Buppekategorien',
+	'categorytree-mode-categories' => 'bloot Kategorien',
+	'categorytree-mode-pages' => 'Sieden buute Bielden',
+	'categorytree-mode-all' => 'aal Sieden',
+	'categorytree-collapse' => 'ienklappe',
+	'categorytree-expand' => 'uutklappe',
+	'categorytree-load' => 'leede',
+	'categorytree-loading' => 'leede ...',
+	'categorytree-nothing-found' => 'Niks fuunen',
+	'categorytree-no-subcategories' => 'Neen Unnerkategorien',
+	'categorytree-no-pages' => 'Neen Sieden of Unnerkategorien',
+	'categorytree-not-found' => 'Kategorie \'\'$1\'\' nit fuunen',
+	'categorytree-error' => 'Probleme bie dät Leeden fon do Doaten.',
+	'categorytree-retry' => 'Täif ieuwen un fersäik et dan fon näien.',
+	'categorytree-show-list' => 'Wies as Lieste',
+	'categorytree-show-tree' => 'Wies as Boom',
+	'categorytree-too-many-subcats' => 'Unnerkategorien konnen nit as Boom deerstoald wäide, deer dät toufuul sunt.',
+);
