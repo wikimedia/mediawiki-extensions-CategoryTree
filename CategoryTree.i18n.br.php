847f73435a86aa67e36c5fb1b2ec7a075170742d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-16 23:54:39 +0000

    Localisation updates from Betawiki (2007-12-17 0:15 CET). * Fixes and additions to 42 extensions for bg, br, el, gl, loz, nl, oc, stq

--- CategoryTree.i18n.br.php
+++ CategoryTree.i18n.br.php
@@ -18,19 +18,32 @@
   Free Documentation License". 
  */
 
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
 $messages = array(
-	'categorytree' => 'Gwezennadur ar rummadoù',
-	'categorytree-tab' => 'Gwezennadur',
-	'categorytree-category' => 'Rummad',
-	'categorytree-go' => 'Diskouez ar gwezennadur',
-	'categorytree-mode-categories' => 'Rummadoù hepken',
-	'categorytree-mode-all' => 'an holl bajennoù',
-	'categorytree-load' => 'kargañ',
-	'categorytree-loading' => 'o kargañ',
+	'categorytree'                  => 'Gwezennadur ar rummadoù',
+	'categorytree-tab'              => 'Gwezennadur',
+	'categorytree-header'           => "Merkit anv ur rummad evit gwelet petra zo ennañ e stumm ur gwezennadur. 
+Notit e rankit kaout an arc'hwelioù JavaScript araokaet anvet AJAX.
+M'eo kozh-mat stumm ho merdeer pe m'eo diweredekaet JavaScript ganeoc'h, ne'z aio ket en-dro.",
+	'categorytree-category'         => 'Rummad',
+	'categorytree-go'               => 'Diskouez ar gwezennadur',
+	'categorytree-parents'          => 'Usrummadoù',
+	'categorytree-mode-categories'  => 'Rummadoù hepken',
+	'categorytree-mode-pages'       => 'Pajennoù hep ar skeudennoù',
+	'categorytree-mode-all'         => 'an holl bajennoù',
+	'categorytree-collapse'         => 'Serriñ',
+	'categorytree-expand'           => 'Dispakañ',
+	'categorytree-load'             => 'kargañ',
+	'categorytree-loading'          => 'o kargañ',
+	'categorytree-nothing-found'    => 'Netra bet kavet',
 	'categorytree-no-subcategories' => 'isrummad ebet',
-	'categorytree-error' => 'Ur gudenn zo bet e-ser kargañ ar roadennoù.',
-	'categorytree-retry' => 'Gortozit un tamm ha klaskit en-dro.',
-	'categorytree-show-list' => 'Diskouez er mod roll',
-	'categorytree-show-tree' => 'Diskouez er mod gwezennadur',
-	'categorytree-too-many-subcats' => 'N\'haller ket diskouez an isrummadoù er mod roll, re zo anezho.',
+	'categorytree-no-pages'         => 'Pennad ebet hag isrummad ebet',
+	'categorytree-not-found'        => "N'eo ket bet kavet ar rummad <i>$1</i>",
+	'categorytree-error'            => 'Ur gudenn zo bet e-ser kargañ ar roadennoù.',
+	'categorytree-retry'            => 'Gortozit un tamm ha klaskit en-dro.',
+	'categorytree-show-list'        => 'Diskouez er mod roll',
+	'categorytree-show-tree'        => 'Diskouez er mod gwezennadur',
+	'categorytree-too-many-subcats' => "N'haller ket diskouez an isrummadoù er mod roll, re zo anezho.",
 );
