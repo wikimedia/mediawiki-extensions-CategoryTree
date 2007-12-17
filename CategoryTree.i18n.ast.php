2ebc94ec0c50af0a94c086bfe2e6a695e689f539
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-17 22:42:52 +0000

    Localisation updates from Betawiki (2007-12-17 23:09 CET). * Fixes and additions to 37 extensions for ar, ast, el, fr, gl, hsb, nl, stq, sv

---
+++ CategoryTree.i18n.ast.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+/** Asturian (Asturianu)
+ * @author SPQRobin
+ */
+$messages = array(
+	'categorytree'                  => 'Árbole de categoríes',
+	'categorytree-tab'              => 'Árbole',
+	'categorytree-header'           => "Introduz una categoría pa ver el so conteníu estructuráu en forma d'árbole. Recuerda que pa ello necesítase la erbía AJAX de JavaScript. Si tienes un navegador mui antiguu o'l JavaScript desactiváu, nun va funcionar.",
+	'categorytree-category'         => 'Categoría',
+	'categorytree-go'               => 'Amosar árbole',
+	'categorytree-parents'          => 'Categoríes superiores',
+	'categorytree-mode-categories'  => 'categoríes namái',
+	'categorytree-mode-pages'       => 'páxines sacante les imáxenes',
+	'categorytree-mode-all'         => 'toles páxines',
+	'categorytree-collapse'         => 'esconder',
+	'categorytree-expand'           => 'espandir',
+	'categorytree-load'             => 'cargar',
+	'categorytree-loading'          => 'cargando',
+	'categorytree-nothing-found'    => "nun s'atopó nada",
+	'categorytree-no-subcategories' => 'nun hai subcategoríes',
+	'categorytree-no-pages'         => 'ensin páxines nin subcategoríes',
+	'categorytree-not-found'        => "Nun s'atopó la categoría <i>$1</i>",
+	'categorytree-error'            => 'Hebo un problema al cargar los datos.',
+	'categorytree-retry'            => 'Por favor, espera unos momentos y inténtalo otra vuelta.',
+	'categorytree-show-list'        => 'Amosar como llista',
+	'categorytree-show-tree'        => 'Amosar como árbole',
+	'categorytree-too-many-subcats' => "Nun se puen amosar les subcategoríes en forma d'árbole porque hai demasiaes.",
+);
