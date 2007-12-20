0133dd2b1032d8ca4cf0ae944125c9d398ec23d3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-20 22:50:57 +0000

    Localisation updates from Betawiki (2007-12-20 23:25 CET). * Fixes and additions to 49 extensions for arm bg, el, fi, gl, hr, hu, loz, nl, ru, stq, sv, to

---
+++ CategoryTree.i18n.to.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+/** Tonga (faka-Tonga)
+ * @author SPQRobin
+ */
+$messages = array(
+	'categorytree'          => 'Fuʻuʻakau faʻahinga',
+	'categorytree-category' => 'Faʻahinga',
+	'categorytree-go'       => 'ʻAsi mai',
+	'categorytree-collapse' => 'holo',
+	'categorytree-expand'   => 'fano',
+);
