a0a3aab18d0e3d8a37ff9d83f9420aeb750c9720
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-22 12:19:50 +0000

    Localisation updates for extensions from Betawiki (2007-12-22 12:25 CET)

--- CategoryTree.i18n.bg.php
+++ CategoryTree.i18n.bg.php
@@ -1,38 +1,38 @@
 <?php
-
 /**
  * Internationalisation file for the CategoryTree extension
  *
  * @addtogroup Extensions
  * @author Daniel Kinzler, brightbyte.de
  * @copyright © 2006 Daniel Kinzler
- * @Bulgarian translation © 2007 Dimitar Tsonev (Gregg), crossword.crozzword.com
  * @licence GNU General Public Licence 2.0 or later
  */
 
-$messages['categorytree']= 'Дърво на категориите';
-$messages['categorytree-tab']= 'Дърво';
-$messages['categorytree-header']= 'Въведете категория, за да видите съдържанието й в дървовиден вид от категории. Имайте предвид, че това изисква допълнителна JavaScript-функционалност, позната като AJAX. Тази възможност не може да бъде използвана ако използвате стар браузър или сте изключили поддържането на JavaScript.';
-
-$messages['categorytree-category']= 'Категория';
-$messages['categorytree-go']= 'Показване';
-$messages['categorytree-parents']= 'Родителски категории';
-
-$messages['categorytree-mode-categories']= 'само категории';
-$messages['categorytree-mode-pages']= 'страници, без файлове';
-$messages['categorytree-mode-all']= 'всички страници';
-
-$messages['categorytree-collapse']= 'събиране';
-$messages['categorytree-expand']= 'разпъване';
-$messages['categorytree-load']= 'зареждане';
-$messages['categorytree-loading']= 'зареждане';
-$messages['categorytree-nothing-found']= 'няма открити поддиректории';
-$messages['categorytree-no-subcategories']= 'няма подкатегории';
-$messages['categorytree-no-pages']= 'няма страници или подкатегории';
-$messages['categorytree-not-found']= "Не беше намерена категория <i>$1</i>";
-$messages['categorytree-error'] = 'Възникна проблем при зареждане на информацията.';
-$messages['categorytree-retry'] = 'Изчакайте малко и опитайте отново.';
-
-$messages['categorytree-show-list'] = "Показване като списък";
-$messages['categorytree-show-tree'] = "Показване като дърво";
-$messages['categorytree-too-many-subcats'] = "Съществуват твърде много подкатегории, невъзможно е да бъдат показани като дърво.";
+/** Bulgarian (Български)
+ * @author Spiritia
+ * @author Dimitar Tsonev (Gregg), crossword.crozzword.com
+ */
+$messages = array(
+	'categorytree'                  => 'Дърво на категориите',
+	'categorytree-tab'              => 'Дърво',
+	'categorytree-header'           => 'Въведете категория, за да видите съдържанието й в дървовиден вид от категории. Имайте предвид, че това изисква допълнителна JavaScript-функционалност, позната като AJAX. Тази възможност не може да бъде използвана, ако използвате стар браузър или сте изключили поддържането на JavaScript.',
+	'categorytree-category'         => 'Категория',
+	'categorytree-go'               => 'Показване',
+	'categorytree-parents'          => 'Родителски категории',
+	'categorytree-mode-categories'  => 'само категории',
+	'categorytree-mode-pages'       => 'страници, без файлове',
+	'categorytree-mode-all'         => 'всички страници',
+	'categorytree-collapse'         => 'събиране',
+	'categorytree-expand'           => 'разпъване',
+	'categorytree-load'             => 'зареждане',
+	'categorytree-loading'          => 'зареждане',
+	'categorytree-nothing-found'    => 'няма открити подкатегории',
+	'categorytree-no-subcategories' => 'няма подкатегории',
+	'categorytree-no-pages'         => 'няма страници или подкатегории',
+	'categorytree-not-found'        => 'Не беше намерена категория <i>$1</i>',
+	'categorytree-error'            => 'Възникна проблем при зареждане на информацията.',
+	'categorytree-retry'            => 'Изчакайте малко и опитайте отново.',
+	'categorytree-show-list'        => 'Показване като списък',
+	'categorytree-show-tree'        => 'Показване като дърво',
+	'categorytree-too-many-subcats' => 'Съществуват твърде много подкатегории, невъзможно е да бъдат показани като дърво.',
+);
