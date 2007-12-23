2e46dbb7fcf570040939a4c5a34635d63b1ca083
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-23 11:21:34 +0000

    Localisation updates for extensions from Betawiki (2007-12-23 11:21 CET)

---
+++ CategoryTree.i18n.lb.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ *
+
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+ */
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages = array(
+	'categorytree-mode-all'         => 'all Säiten',
+	'categorytree-collapse'         => 'zouklappen',
+	'categorytree-expand'           => 'opklappen',
+	'categorytree-nothing-found'    => 'Näischt fonnt',
+	'categorytree-no-subcategories' => 'Keng Ënnerkategorien',
+	'categorytree-not-found'        => "Kategorie ''$1'' net fonnt",
+	'categorytree-show-list'        => 'Als Lescht uweisen',
+);
