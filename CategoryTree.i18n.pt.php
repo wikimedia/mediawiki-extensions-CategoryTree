ce533b16efc786a49a5168c7f7212e4edb7bcb08
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-27 11:51:22 +0000

    Localisation updates for extension messages from Betawiki (2007-12-27 12:05 CET)

--- CategoryTree.i18n.pt.php
+++ CategoryTree.i18n.pt.php
@@ -2,60 +2,62 @@
 /*
   Internationalisation file for the CategoryTree extension
   Copyright (C) 2006, Daniel Kinzler, brightbyte.de
-  Portuguese by Waldir Pimenta & Lugusto
- 
+
   This file is licensed under the GPL and the GFDL:
-  
-  
+
+
   Permission is granted to copy, distribute and/or modify this document
   under the terms of the GNU Free Documentation License, Version 1.2
   or any later version published by the Free Software Foundation;
   with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
   Texts.  A copy of the license is included in the section entitled "GNU
   Free Documentation License".
-  
-  
+
+
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
   as published by the Free Software Foundation; either version 2
   of the License, or (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
   */
-  
-$messages['categorytree']= 'Árvore de categorias';
-$messages['categorytree-tab']= 'Árvore';
-$messages['categorytree-header']=
-'Insira o nome de uma categoria para ver seu conteúdo como uma estrutura de "árvore". 
+
+/** Portuguese (Português)
+ * @author Waldir Pimenta
+ * @author Lugusto
+ * @author 555
+ */
+$messages = array(
+	'categorytree'                  => 'Árvore de categorias',
+	'categorytree-tab'              => 'Árvore',
+	'categorytree-header'           => 'Insira o nome de uma categoria para ver seu conteúdo como uma estrutura de "árvore".
 Note que isso requer funcionalidades avançadas de JavaScript (como, por exemplo, AJAX).
 Caso o seu navegador seja razoavelmente antigo, ou, caso JavaScript esteja desabilitado em seu navegador, isto não funcionará.
-';
-  
-$messages['categorytree-category']= 'Categoria';
-$messages['categorytree-go']= 'Exibir Árvore';
-$messages['categorytree-parents']= 'Categorias superiores';
-  
-$messages['categorytree-mode-categories']= 'mostrar só categorias';
-$messages['categorytree-mode-pages']= 'páginas com exceção de imagens';
-$messages['categorytree-mode-all']= 'todas as páginas';
-  
-$messages['categorytree-collapse']= 'ocultar';
-$messages['categorytree-expand']= 'expandir';
-$messages['categorytree-load']= 'carregar';
-$messages['categorytree-loading']= 'carregando';
-$messages['categorytree-nothing-found']= 'Sentimos muito, não se encontrou nada';
-$messages['categorytree-no-subcategories']= 'sem subcategorias.';
-$messages['categorytree-no-pages']= 'sem artigos nem subcategorias.';
-$messages['categorytree-not-found']= "A categoria ''\$1'' não foi encontrada";
-
-$messages['categorytree-show-list'] = "Exibir como lista";
-$messages['categorytree-show-tree'] = "Exibir como árvore";
-$messages['categorytree-too-many-subcats'] = "Não é possível exibir as subcategorias no modo de árvore, existem muitas subcategorias.";
-
+',
+	'categorytree-category'         => 'Categoria',
+	'categorytree-go'               => 'Exibir Árvore',
+	'categorytree-parents'          => 'Categorias superiores',
+	'categorytree-mode-categories'  => 'mostrar só categorias',
+	'categorytree-mode-pages'       => 'páginas com exceção de imagens',
+	'categorytree-mode-all'         => 'todas as páginas',
+	'categorytree-collapse'         => 'ocultar',
+	'categorytree-expand'           => 'expandir',
+	'categorytree-load'             => 'carregar',
+	'categorytree-loading'          => 'carregando',
+	'categorytree-nothing-found'    => 'Sentimos muito, não se encontrou nada',
+	'categorytree-no-subcategories' => 'sem subcategorias.',
+	'categorytree-no-pages'         => 'sem páginas nem subcategorias',
+	'categorytree-not-found'        => "A categoria ''$1'' não foi encontrada",
+	'categorytree-error'            => 'Problema ao carregar os dados.',
+	'categorytree-retry'            => 'Por gentileza, aguarde um momento e tente novamente.',
+	'categorytree-show-list'        => 'Exibir como lista',
+	'categorytree-show-tree'        => 'Exibir como árvore',
+	'categorytree-too-many-subcats' => 'Não é possível exibir as subcategorias no modo de árvore, existem muitas subcategorias.',
+);
