57142f5c1a16301f3f3a5f9207ee72c00d2bc2f3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-03 22:34:13 +0000

    Localisation updates for extension messages from Betawiki (2008-01-03 23:09 CET)

---
+++ CategoryTree.i18n.an.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ *
+
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+ */
+
+/** Aragonese (Aragonés)
+ * @author Juanpabl
+ */
+$messages = array(
+	'categorytree'                  => 'Árbol de categorías',
+	'categorytree-tab'              => 'Árbol',
+	'categorytree-header'           => "Escriba un nombre de categoría ta beyer os suyos contenius en forma d'árbol. Pare cuenta que ista pachina requiere as funzions JavaScriptz abanzatas conoixitas como AJAX. Si tiene un nabegador antigo, u tiene desautibato JavaScript, a pachina no funzionará.",
+	'categorytree-category'         => 'Categoría',
+	'categorytree-go'               => "Amostrar l'Árbol",
+	'categorytree-parents'          => 'Categorías mais',
+	'categorytree-mode-categories'  => 'amostrar nomás categorías',
+	'categorytree-mode-pages'       => 'pachinas pero no imachens',
+	'categorytree-mode-all'         => 'todas as pachinas',
+	'categorytree-collapse'         => 'amagar',
+	'categorytree-expand'           => 'amostrar',
+	'categorytree-load'             => 'cargar',
+	'categorytree-loading'          => 'cargando',
+	'categorytree-nothing-found'    => "No s'ha trobato cosa",
+	'categorytree-no-subcategories' => 'no bi ha subcategorías',
+	'categorytree-no-pages'         => 'No bi ha articlos ni subcategorías',
+	'categorytree-not-found'        => "Categoría ''$1'' no trobata",
+	'categorytree-error'            => 'Error en cargar os datos',
+	'categorytree-retry'            => 'Por fabor, aspere bels intes y prebe de nuebas.',
+	'categorytree-show-list'        => 'Amostrar como una lista',
+	'categorytree-show-tree'        => 'Amostrar como un árbol',
+	'categorytree-too-many-subcats' => "No se pueden amostrar as subcategorías como un árbol porque bi'n ha masiadas.",
+);
