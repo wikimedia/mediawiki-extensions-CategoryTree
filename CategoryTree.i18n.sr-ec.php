746d40a24ab1f8ac3181f9acb7f9d9b3b493d31d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-04 18:35:36 +0000

    Localisation updates for extension messages from Betawiki (2008-01-04 19:09 CET)

---
+++ CategoryTree.i18n.sr-ec.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author editors of sl.wikipedia
+ * @copyright  2006 editors of sl.wikipedia
+ * @licence GNU Free Documentation License, Version 1.2
+ * 
+ 
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License". 
+ */
+
+/** ћирилица (ћирилица)
+ * @author Sasa Stefanovic
+ */
+$messages = array(
+	'categorytree'                  => 'Дрво категорија',
+	'categorytree-tab'              => 'Дрво',
+	'categorytree-category'         => 'Категорија',
+	'categorytree-go'               => 'Прикажи дрво',
+	'categorytree-mode-categories'  => 'само категорије',
+	'categorytree-mode-pages'       => 'странице изузев слика',
+	'categorytree-mode-all'         => 'све странице',
+	'categorytree-collapse'         => 'сакриј',
+	'categorytree-expand'           => 'прикажи',
+	'categorytree-load'             => 'учитај',
+	'categorytree-loading'          => 'учитавање',
+	'categorytree-nothing-found'    => 'ништа није пронађено',
+	'categorytree-no-subcategories' => 'нема подкатегорија',
+	'categorytree-no-pages'         => 'нема страница или подкатегорија',
+	'categorytree-not-found'        => 'Категорија <i>$1</i> није пронађена',
+	'categorytree-error'            => 'Проблем при учитавању података.',
+	'categorytree-retry'            => 'Молимо сачекајте тренутак и покушајте поново',
+	'categorytree-show-list'        => 'Прикажи као списак',
+	'categorytree-show-tree'        => 'Прикажи као дрво',
+	'categorytree-too-many-subcats' => 'Не могу приказати подкатегорије као дрво, има их превише.',
+);
