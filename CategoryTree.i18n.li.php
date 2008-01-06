08c0ffd28edbc0748ba512ce430c847d3388ac70
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-06 11:16:11 +0000

    Localisation updates for extension messages from Betawiki (2008-01-05 11:56 CET)

---
+++ CategoryTree.i18n.li.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Internationalisation file for the CategoryTree extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Betawiki editors
+ * @copyright  2007 Betawiki editors
+ * @licence GNU Free Documentation License, Version 1.2
+ *
+
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.2
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled "GNU
+  Free Documentation License".
+ */
+
+/** Limburgish (Limburgs)
+ * @author Ooswesthoesbes
+ */
+$messages = array(
+	'categorytree'                  => 'Categorieboum',
+	'categorytree-tab'              => 'Boum',
+	'categorytree-header'           => "Gaef 'ne categorienaam in om de inhoud es 'ne boumstructuur te bekieke.
+Let op: deze functie gebroek JavaScript-functionaliteit dae bekindj steit es AJAX.
+Esse 'ne erg verajerdje browser höbs of JavaScript steit oet, den werk dees functie neet.",
+	'categorytree-category'         => 'Categorie',
+	'categorytree-go'               => 'Laje',
+	'categorytree-parents'          => 'Baoveligkendje categorië',
+	'categorytree-mode-categories'  => 'allein categorië',
+	'categorytree-mode-pages'       => 'gein aafbeildinge',
+	'categorytree-mode-all'         => "alle pazjena's",
+	'categorytree-collapse'         => 'instorte',
+	'categorytree-expand'           => 'oetklappe',
+	'categorytree-load'             => 'laje',
+	'categorytree-loading'          => "aan 't laje",
+	'categorytree-nothing-found'    => 'Dees categorie haet gein subcategorië.',
+	'categorytree-no-subcategories' => 'Gein subcategorië.',
+	'categorytree-no-pages'         => "Gein pazjena's of óngercategorië.",
+	'categorytree-not-found'        => "Categorie ''$1'' neet gevónje",
+	'categorytree-error'            => "Perbleem bie 't laje van de gegaeves.",
+	'categorytree-retry'            => "Wach estebleef ef en perbeer 't den opnuuj.",
+	'categorytree-show-list'        => 'Toean es lies.',
+	'categorytree-show-tree'        => 'Toean es boumstructuur.',
+	'categorytree-too-many-subcats' => "Kin de óngercategorië neet es boumstructuur toeane, d'r zeen d'rs te väöl.",
+);
