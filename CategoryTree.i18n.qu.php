14ea32e8ac5cf931e16c61e12cfee881e2c3d893
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-16 16:14:56 +0000

    Localisation updates for extension messages from Betawiki (2008-01-16 16:44 CET)

---
+++ CategoryTree.i18n.qu.php
@@ -0,0 +1,28 @@
+<?php
+/** Quechua (Runa Simi)
+ * @author AlimanRuna
+ */
+$messages = array(
+	'categorytree'                  => "Katiguriya sach'a (CategoryTree)",
+	'categorytree-tab'              => "Sach'a",
+	'categorytree-header'           => "Katiguriya sutita yaykuchiy samiqninta sach'a hinata rikunaykipaq.
+Musyariy, kaytaqa AJAX nisqa sapaq JavaScript ruranallawanmi llamk'achiyta atinki. Mawk'a wamp'unawanqa icha JavaScript nisqaman ama nispaqa manam atinkichu.",
+	'categorytree-category'         => 'Katiguriya',
+	'categorytree-go'               => "Sach'ata rikuchiy",
+	'categorytree-parents'          => 'Mama katiguriyakuna',
+	'categorytree-mode-categories'  => 'Katiguriyakunalla',
+	'categorytree-mode-pages'       => "p'anqakuna amataq rikchakuna",
+	'categorytree-mode-all'         => "tukuy p'anqakuna",
+	'categorytree-collapse'         => 'pakay',
+	'categorytree-expand'           => 'rikuchiy',
+	'categorytree-load'             => 'chaqnay',
+	'categorytree-loading'          => 'chaqnaspa',
+	'categorytree-nothing-found'    => 'manam imapas tarisqachu',
+	'categorytree-no-subcategories' => 'mana ima urin katiguriyapas',
+	'categorytree-no-pages'         => 'mana ima urin qillqapas ni katiguriyapas',
+	'categorytree-not-found'        => '<i>$1</i> sutiyuq katiguriyaqa manam tarisqachu',
+	'categorytree-error'            => 'Manam atinichu willakunata chaqnayta.',
+	'categorytree-retry'            => 'Asllata suyaspa musuqmanta ruraykachay.',
+	'categorytree-show-list'        => 'Sutisuyuta rikuchiy',
+	'categorytree-show-tree'        => "Sach'a hinata rikuchiy",
+);
