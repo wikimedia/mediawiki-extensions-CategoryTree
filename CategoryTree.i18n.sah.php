df3ba651510502d5c8c7867d8c9f900604db6d2d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-17 09:50:18 +0000

    Localisation updates for extension messages from Betawiki (2008-01-17 09:50 CET)

---
+++ CategoryTree.i18n.sah.php
@@ -0,0 +1,9 @@
+<?php
+/** Yakut (Саха тыла)
+ * @author HalanTul
+ */
+$messages = array(
+	'categorytree-category' => 'Категория',
+	'categorytree-parents'  => 'Төрөппүттэрэ',
+	'categorytree-mode-all' => 'бары сирэйдэр',
+);
