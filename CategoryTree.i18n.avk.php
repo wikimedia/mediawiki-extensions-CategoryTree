991d9cf462744094c1908119d70f611752a556ae
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-17 00:44:37 +0000

    Localisation updates for extension messages from Betawiki

---
+++ CategoryTree.i18n.avk.php
@@ -0,0 +1,30 @@
+<?php
+/** Kotava (Kotava)
+ * @author Wikimistusik
+ */
+$messages = array(
+	'categorytree'                  => 'LomaAal',
+	'categorytree-tab'              => 'Aal',
+	'categorytree-header'           => 'Ta wira va aaldrekoraf cek va lomayolt bazel !
+Stragal da batcoba va AJAX JavaScript fliaca kucilar.
+Ede va guazafi exulesiki favel oke ede JavaScript fliaceem tir metegis, batcoba me guyundeter.',
+	'categorytree-category'         => 'Loma',
+	'categorytree-go'               => 'Nedira va aal',
+	'categorytree-parents'          => 'Veylomeem',
+	'categorytree-mode-categories'  => 'Anton lomeem',
+	'categorytree-mode-pages'       => 'Bueem rade ewaveem',
+	'categorytree-mode-all'         => 'bueem',
+	'categorytree-collapse'         => 'koatcera',
+	'categorytree-expand'           => 'divatcera',
+	'categorytree-load'             => 'vajara',
+	'categorytree-loading'          => 'vajas',
+	'categorytree-nothing-found'    => 'mek trasiks',
+	'categorytree-no-subcategories' => 'meka volveyloma',
+	'categorytree-no-pages'         => 'meku bu oku volveyloma',
+	'categorytree-not-found'        => '<i>$1</i> loma metrasiyina',
+	'categorytree-error'            => 'Zvak remi origvajara.',
+	'categorytree-retry'            => 'Vay kemel aze tolyawal !',
+	'categorytree-show-list'        => 'Vexalakorafa nedira',
+	'categorytree-show-tree'        => 'Aalkorafa nedira',
+	'categorytree-too-many-subcats' => 'Aalkorafa nedira me rotir kire slika volveyloma tid.',
+);
