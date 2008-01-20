39eaeb20791d9e354a2b2e9b29b2e046f6e8f570
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-20 19:39:32 +0000

    Localisation updates for extension messages from Betawiki (2008-01-20 20:21 CET)

---
+++ CategoryTree.i18n.te.php
@@ -0,0 +1,12 @@
+<?php
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages = array(
+	'categorytree'                  => 'వర్గవృక్షం',
+	'categorytree-category'         => 'వర్గం',
+	'categorytree-mode-all'         => 'అన్ని పేజీలు',
+	'categorytree-nothing-found'    => 'ఏమీ లేవు',
+	'categorytree-no-subcategories' => 'ఉపవర్గాలు లేవు',
+);
+
