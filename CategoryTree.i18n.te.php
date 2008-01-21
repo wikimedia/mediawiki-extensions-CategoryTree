7f5466bb9116572e545be2d62963c3fb9183601a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-21 16:57:03 +0000

    Localisation updates for extension messages from Betawiki (2008-01-21 17:19 CET)

--- CategoryTree.i18n.te.php
+++ CategoryTree.i18n.te.php
@@ -5,8 +5,11 @@
 $messages = array(
 	'categorytree'                  => 'వర్గవృక్షం',
 	'categorytree-category'         => 'వర్గం',
+	'categorytree-mode-categories'  => 'వర్గాలు మాత్రమే',
 	'categorytree-mode-all'         => 'అన్ని పేజీలు',
 	'categorytree-nothing-found'    => 'ఏమీ లేవు',
 	'categorytree-no-subcategories' => 'ఉపవర్గాలు లేవు',
+	'categorytree-show-list'        => 'జాబితాగా చూపించు',
+	'categorytree-show-tree'        => 'వృక్షంగా చూపించు',
 );
 
