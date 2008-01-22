16c9719e2e6e1ee6f58abfa371750d15b864955e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-22 21:43:14 +0000

    Localisation updates for extension messages from Betawiki (2008-01-22 22:34 CET)

---
+++ CategoryTree.i18n.ps.php
@@ -0,0 +1,9 @@
+<?php
+/** Pashto (پښتو)
+ * @author Ahmed-Najib-Biabani-Ibrahimkhel
+ */
+$messages = array(
+	'categorytree-category' => 'وېشنيزه',
+	'categorytree-mode-all' => 'ټول مخونه',
+);
+
--- CategoryTree.i18n.sah.php
+++ CategoryTree.i18n.sah.php
@@ -3,7 +3,25 @@
  * @author HalanTul
  */
 $messages = array(
-	'categorytree-category' => 'Категория',
-	'categorytree-parents'  => 'Төрөппүттэрэ',
-	'categorytree-mode-all' => 'бары сирэйдэр',
+	'categorytree'                  => 'Категориялар мас курдук',
+	'categorytree-tab'              => 'Мас курдук',
+	'categorytree-category'         => 'Категория',
+	'categorytree-go'               => 'Көрдөр',
+	'categorytree-parents'          => 'Төрөппүттэрэ',
+	'categorytree-mode-categories'  => 'категориялар эрэ',
+	'categorytree-mode-pages'       => 'ойуулартан ураты',
+	'categorytree-mode-all'         => 'бары сирэйдэр',
+	'categorytree-collapse'         => 'сап',
+	'categorytree-expand'           => 'тэнит',
+	'categorytree-load'             => 'киллэр',
+	'categorytree-loading'          => 'киллэрии',
+	'categorytree-nothing-found'    => 'бу категория подкатегорията суох',
+	'categorytree-no-subcategories' => 'субкатегорията суох',
+	'categorytree-no-pages'         => 'ыстатыйата эбэтэр субкатегорията суох',
+	'categorytree-not-found'        => '<i>$1</i> категория көстүбэтэ',
+	'categorytree-retry'            => 'Кыратык кэтэһэ түһэн баран өссө боруобалаа',
+	'categorytree-show-list'        => 'Тиһик (Испииһэк)',
+	'categorytree-show-tree'        => 'Мас курдук',
+	'categorytree-too-many-subcats' => 'Субкатегориялары мас курдук көрдөрөр табыллыбата - элбэхтэрэ бэрт.',
 );
+
