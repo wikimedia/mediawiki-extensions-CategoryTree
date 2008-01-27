fa1cdeea0b7217d3bd8113685eb8e87d73c9640f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-27 15:34:48 +0000

    Localisation updates for extension messages from Betawiki (2008-01-27 16:12 CET)

--- CategoryTree.i18n.vo.php
+++ CategoryTree.i18n.vo.php
@@ -1,42 +1,30 @@
 <?php
-
-/**
- * Internationalisation file for the CategoryTree extension
- *
- * @package MediaWiki
- * @subpackage Extensions
- * @author Betawiki editors
- * @copyright  2007 Betawiki editors
- * @licence GNU Free Documentation License, Version 1.2
- * 
- 
-  Permission is granted to copy, distribute and/or modify this document
-  under the terms of the GNU Free Documentation License, Version 1.2
-  or any later version published by the Free Software Foundation;
-  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
-  Texts.  A copy of the license is included in the section entitled "GNU
-  Free Documentation License". 
- */
-
 /** Volapük (Volapük)
- * @author Malafaya
  * @author Smeira
+ * @author Malafaya
  */
 $messages = array(
 	'categorytree'                  => 'KladaBim',
 	'categorytree-tab'              => 'Bim',
+	'categorytree-header'           => 'Penolös kladanemi ad logön ninädi klada as bimabinod. Küpälolös, das atos flagon dili ela JavaScript labü nem: AJAX. No oplöpon if labol bevüresodanafömi vönädik, ud if enemogüköl eli JavaScript.',
 	'categorytree-category'         => 'Klad',
 	'categorytree-go'               => 'Jonolöd Bimi',
 	'categorytree-parents'          => 'Pals',
 	'categorytree-mode-categories'  => 'Te klads',
 	'categorytree-mode-pages'       => 'pads pläamü magods',
 	'categorytree-mode-all'         => 'pads valik',
+	'categorytree-collapse'         => 'brefükön',
+	'categorytree-expand'           => 'stäänükön',
+	'categorytree-load'             => 'lodön',
+	'categorytree-loading'          => 'lodam',
 	'categorytree-nothing-found'    => 'nos petuvon',
 	'categorytree-no-subcategories' => 'donaklads nonik',
 	'categorytree-no-pages'         => 'pads e donaklads noniks',
 	'categorytree-not-found'        => 'Klad: <i>$1</i> no petuvöl',
+	'categorytree-error'            => 'No eplöpos ad lodön nünis.',
 	'categorytree-retry'            => 'Stebedolös, begö! timüli e steifülolös dönu.',
 	'categorytree-show-list'        => 'Jonön as lised',
 	'categorytree-show-tree'        => 'Jonön as bim',
 	'categorytree-too-many-subcats' => 'Donaklads no kanons pajonön as bim, bi binons tu mödiks.',
 );
+
