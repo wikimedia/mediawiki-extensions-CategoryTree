fb327a6c2f6aa78f7fa7c4946b46299b1750aa1a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-30 23:08:34 +0000

    Localisation updates for extension messages from Betawiki

---
+++ CategoryTree.i18n.bn.php
@@ -0,0 +1,31 @@
+<?php
+/** Bengali (বাংলা)
+ * @author Zaheen
+ */
+$messages = array(
+	'categorytree'                  => 'বিষয়শ্রেণীবৃক্ষ',
+	'categorytree-tab'              => 'বৃক্ষ',
+	'categorytree-header'           => 'যে বিষয়শ্রেণীটির অন্তর্ভুক্ত বিষয়বস্তু বৃক্ষাকারে দেখতে চান, সেটির নাম প্রবেশ করান।
+লক্ষ্য করুন এর জন্য এজ্যাক্স নামের একটি অগ্রসর জাভাস্ক্রিপ্ট কৌশল ব্যবহার করা হয়।
+যদি আপনার ব্রাউজারটি খুব পুরনো হয়, বা যদি জাভাস্ক্রিপ্ট নিষ্ক্রিয় করা থাকে, তবে এটি কাজ করবে না।',
+	'categorytree-category'         => 'বিষয়শ্রেণী',
+	'categorytree-go'               => 'বৃক্ষ দেখানো হোক',
+	'categorytree-parents'          => 'পিতামাতা',
+	'categorytree-mode-categories'  => 'শুধুমাত্র বিষয়শ্রেণী',
+	'categorytree-mode-pages'       => 'ছবি ব্যতীত সব পাতা',
+	'categorytree-mode-all'         => 'সব পাতা',
+	'categorytree-collapse'         => 'গুটিয়ে ফেলা হোক',
+	'categorytree-expand'           => 'প্রসারিত করা হোক',
+	'categorytree-load'             => 'নিয়ে আসা হোক',
+	'categorytree-loading'          => 'নিয়ে আসা হচ্ছে',
+	'categorytree-nothing-found'    => 'কিছু পাওয়া যায়নি',
+	'categorytree-no-subcategories' => 'কোন উপ-বিষয়শ্রেণী নেই',
+	'categorytree-no-pages'         => 'কোন পাতা বা উপ-বিষয়শ্রেণী নেই',
+	'categorytree-not-found'        => '<i>$1</i> বিষয়শ্রেণীটি খুঁজে পাওয়া যায়নি',
+	'categorytree-error'            => 'উপাত্ত নিয়ে আসতে সমস্যা।',
+	'categorytree-retry'            => 'অনুগ্রহ করে একটু অপেক্ষা করে আবার চেষ্টা করুন।',
+	'categorytree-show-list'        => 'তালিকা আকারে দেখানো হোক',
+	'categorytree-show-tree'        => 'বৃক্ষাকারে দেখানো হোক',
+	'categorytree-too-many-subcats' => 'উপবিষয়শ্রেণীগুলি সংখ্যায় খুব বেশি বলে এগুলি বৃক্ষাকারে দেখানো গেল না।',
+);
+
