cd3efc13b93e0f55c3a865340de6f4604ee1c7d3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-03 12:52:56 +0000

    Localisation updates for extension messages from Betawiki (2008-02-03 13:34 CET)

---
+++ CategoryTree.i18n.tg.php
@@ -0,0 +1,11 @@
+<?php
+/** Tajik (Тоҷикӣ)
+ * @author Ibrahim
+ */
+$messages = array(
+	'categorytree-tab'      => 'Дарахт',
+	'categorytree-category' => 'Гурӯҳ',
+	'categorytree-mode-all' => 'ҳамаи саҳифаҳо',
+	'categorytree-error'    => 'Ишкол дар дарёфти иттилоот',
+);
+
