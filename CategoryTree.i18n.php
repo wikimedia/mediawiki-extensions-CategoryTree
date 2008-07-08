1cd5cbec67cd2bf21c04d28a20a4ad3b03ab2cd7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-08 21:45:09 +0000

    Localisation updates for extension messages from Betawiki (2008-07-08 23:21 CEST)

--- CategoryTree.i18n.php
+++ CategoryTree.i18n.php
@@ -133,28 +133,30 @@ $messages['an'] = array(
  * @author Siebrand
  */
 $messages['ar'] = array(
-	'categorytree'                  => 'شجرة تصنيف',
-	'categorytree-legend'           => 'عرض شجرة التصنيف',
-	'categorytree-desc'             => 'إضافة معتمدة على الأجاكس لعرض [[Special:CategoryTree|هيكل التصنيف]] لويكي',
-	'categorytree-header'           => 'أدخل اسم تصنيف لترى محتوياته كتركيب شجري.
+	'categorytree'                      => 'شجرة تصنيف',
+	'categorytree-portlet'              => 'تصنيفات',
+	'categorytree-legend'               => 'عرض شجرة التصنيف',
+	'categorytree-desc'                 => 'إضافة معتمدة على الأجاكس لعرض [[Special:CategoryTree|هيكل التصنيف]] لويكي',
+	'categorytree-header'               => 'أدخل اسم تصنيف لترى محتوياته كتركيب شجري.
 لاحظ أن هذا يتطلب خاصية جافاسكريبت متقدمة معروفة كأجاكس.
 لو كنت تمتلك متصفحا قديما جدا، أو لديك الجافاسكريبت معطلة، فلن تعمل.',
-	'categorytree-category'         => ':تصنيف',
-	'categorytree-go'               => 'عرض الشجرة',
-	'categorytree-parents'          => 'مصنف تحت',
-	'categorytree-mode-categories'  => 'تصنيفات فقط',
-	'categorytree-mode-pages'       => 'الصفحات ماعدا الصور',
-	'categorytree-mode-all'         => 'كل الصفحات',
-	'categorytree-collapse'         => 'ضغط',
-	'categorytree-expand'           => 'فرد',
-	'categorytree-load'             => 'تحميل',
-	'categorytree-loading'          => 'جاري التحميل',
-	'categorytree-nothing-found'    => 'لم يتم العثور على شيء',
-	'categorytree-no-subcategories' => 'لا تصنيفات فرعية',
-	'categorytree-no-pages'         => 'لا صفحات ولا تصنيفات فرعية',
-	'categorytree-not-found'        => 'التصنيف <i>$1</i> لم يتم العثور عليه',
-	'categorytree-error'            => 'مشكلة في تحميل البيانات.',
-	'categorytree-retry'            => 'من فضلك انتظر لحظة وحاول مرة أخرى.',
+	'categorytree-category'             => ':تصنيف',
+	'categorytree-go'                   => 'عرض الشجرة',
+	'categorytree-parents'              => 'مصنف تحت',
+	'categorytree-mode-categories'      => 'تصنيفات فقط',
+	'categorytree-mode-pages'           => 'الصفحات ماعدا الصور',
+	'categorytree-mode-all'             => 'كل الصفحات',
+	'categorytree-collapse'             => 'ضغط',
+	'categorytree-expand'               => 'فرد',
+	'categorytree-load'                 => 'تحميل',
+	'categorytree-loading'              => 'جاري التحميل',
+	'categorytree-nothing-found'        => 'لم يتم العثور على شيء',
+	'categorytree-no-subcategories'     => 'لا تصنيفات فرعية',
+	'categorytree-no-parent-categories' => 'لا تصنيفات أصلية',
+	'categorytree-no-pages'             => 'لا صفحات ولا تصنيفات فرعية',
+	'categorytree-not-found'            => 'التصنيف <i>$1</i> لم يتم العثور عليه',
+	'categorytree-error'                => 'مشكلة في تحميل البيانات.',
+	'categorytree-retry'                => 'من فضلك انتظر لحظة وحاول مرة أخرى.',
 );
 
 /** Assamese (অসমীয়া)
@@ -259,6 +261,8 @@ $messages['bcl'] = array(
  */
 $messages['be-tarask'] = array(
 	'categorytree'                  => 'Дрэва катэгорый',
+	'categorytree-portlet'          => 'Катэгорыі',
+	'categorytree-legend'           => 'Паказаць дрэва катэгорый',
 	'categorytree-desc'             => 'Заснаваная на AJAX прылада для паказу [[Special:CategoryTree|структуры катэгорый]] {{GRAMMAR:родны|{{SITENAME}}}}',
 	'categorytree-category'         => 'Катэгорыя:',
 	'categorytree-go'               => 'Паказаць дрэва',
@@ -1514,6 +1518,7 @@ Eger şolğışıñız öte eski, nemese JavaScript öşirilgen bolsa, bul iske 
  */
 $messages['km'] = array(
 	'categorytree'                  => 'មែកធាងនៃចំណាត់ថ្នាក់ក្រុម',
+	'categorytree-portlet'          => 'ចំនាត់ថ្នាក់ក្រុម',
 	'categorytree-legend'           => 'បង្ហាញមែកធាងចំនាត់ថ្នាក់ក្រុម',
 	'categorytree-category'         => 'ចំណាត់ថ្នាក់ក្រុម:',
 	'categorytree-go'               => 'បង្ហាញដើមឈើ',
@@ -2159,28 +2164,31 @@ $messages['ps'] = array(
  * @author Siebrand
  */
 $messages['pt'] = array(
-	'categorytree'                  => 'Árvore de categorias',
-	'categorytree-legend'           => 'Exibir a árvore de categorias',
-	'categorytree-desc'             => 'Acessório (gadget) baseado em AJAX que apresenta a [[Special:CategoryTree|estrutura]] de um wiki',
-	'categorytree-header'           => 'Insira o nome de uma categoria para ver seu conteúdo como uma estrutura de "árvore".
+	'categorytree'                      => 'Árvore de categorias',
+	'categorytree-portlet'              => 'Categorias',
+	'categorytree-legend'               => 'Exibir a árvore de categorias',
+	'categorytree-desc'                 => 'Acessório (gadget) baseado em AJAX que apresenta a [[Special:CategoryTree|estrutura]] de um wiki',
+	'categorytree-header'               => 'Insira o nome de uma categoria para ver seu conteúdo como uma estrutura de "árvore".
 Note que isso requer funcionalidades avançadas de JavaScript (como, por exemplo, AJAX).
 Caso o seu navegador seja razoavelmente antigo, ou, caso JavaScript esteja desabilitado em seu navegador, isto não funcionará.',
-	'categorytree-category'         => 'Categoria',
-	'categorytree-go'               => 'Exibir Árvore',
-	'categorytree-parents'          => 'Categorias superiores',
-	'categorytree-mode-categories'  => 'mostrar só categorias',
-	'categorytree-mode-pages'       => 'páginas com exceção de imagens',
-	'categorytree-mode-all'         => 'todas as páginas',
-	'categorytree-collapse'         => 'ocultar',
-	'categorytree-expand'           => 'expandir',
-	'categorytree-load'             => 'carregar',
-	'categorytree-loading'          => 'carregando',
-	'categorytree-nothing-found'    => 'Sentimos muito, não se encontrou nada',
-	'categorytree-no-subcategories' => 'sem subcategorias.',
-	'categorytree-no-pages'         => 'sem páginas nem subcategorias',
-	'categorytree-not-found'        => "A categoria ''$1'' não foi encontrada",
-	'categorytree-error'            => 'Problema ao carregar os dados.',
-	'categorytree-retry'            => 'Por gentileza, aguarde um momento e tente novamente.',
+	'categorytree-category'             => 'Categoria:',
+	'categorytree-go'                   => 'Exibir Árvore',
+	'categorytree-parents'              => 'Categorias superiores',
+	'categorytree-mode-categories'      => 'mostrar apenas as categorias',
+	'categorytree-mode-pages'           => 'páginas, exceto imagens',
+	'categorytree-mode-all'             => 'todas as páginas',
+	'categorytree-collapse'             => 'ocultar',
+	'categorytree-expand'               => 'expandir',
+	'categorytree-member-counts'        => 'possui {{PLURAL:$1|uma subcategoria|$1 subcategorias}}, {{PLURAL:$2|uma página|$2 páginas}} e {{PLURAL:$3|um ficheiro|$3 ficheiros}}',
+	'categorytree-load'                 => 'carregar',
+	'categorytree-loading'              => 'carregando',
+	'categorytree-nothing-found'        => 'Sentimos muito, não se encontrou nada',
+	'categorytree-no-subcategories'     => 'sem subcategorias',
+	'categorytree-no-parent-categories' => 'não há categorias superiores',
+	'categorytree-no-pages'             => 'sem páginas nem subcategorias',
+	'categorytree-not-found'            => 'A categoria <i>$1</i> não foi encontrada',
+	'categorytree-error'                => 'Problema ao carregar os dados.',
+	'categorytree-retry'                => 'Por gentileza, aguarde um momento e tente novamente.',
 );
 
 /** Quechua (Runa Simi)
