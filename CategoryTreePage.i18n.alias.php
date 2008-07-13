3f3c3744dc405540533102144204a666757ff0de
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-13 13:41:02 +0000

    Localisation updates for extension special pages from Betawiki

--- CategoryTreePage.i18n.alias.php
+++ CategoryTreePage.i18n.alias.php
@@ -17,6 +17,16 @@ $aliases['en'] = array(
 	'CategoryTree' => array( 'CategoryTree' ),
 );
 
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'CategoryTree' => array( 'شجرة تصنيف' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'CategoryTree' => array( 'شجرة تصنيف' ),
+);
+
 /** German
  * @author Daniel Kinzler, brightbyte.de
  */
@@ -24,8 +34,14 @@ $aliases['de'] = array(
 	'CategoryTree' => array( 'Kategorienbaum', 'Kategoriebaum' ),
 );
 
+/** Finnish (Suomi) */
 $aliases['fi'] = array(
 	'CategoryTree' => array( 'Luokkapuu' ),
+);
+
+/** French (Français) */
+$aliases['fr'] = array(
+	'CategoryTree' => array( 'ArbreCatégorie', 'ArbreCategorie', 'Arbre catégorie' ),
 );
 
 /** Hebrew (עברית)
@@ -35,14 +51,33 @@ $aliases['he'] = array(
 	'CategoryTree' => array( 'עץ_קטגוריות' ),
 );
 
+/** Haitian (Kreyòl ayisyen) */
+$aliases['ht'] = array(
+	'CategoryTree' => array( 'KategoriPlant' ),
+);
+
+/** Hungarian (Magyar) */
 $aliases['hu'] = array(
 	'CategoryTree' => array( 'Kategóriafa' ),
 );
 
+/** Latin (Latina) */
 $aliases['la'] = array(
 	'CategoryTree' => array( 'Categoriarum arbor' ),
 );
 
+/** Malay (Bahasa Melayu) */
+$aliases['ms'] = array(
+	'CategoryTree' => array( 'Salasilah_kategori', 'Kategori_asal' ),
+);
+
+/** Dutch (Nederlands) */
 $aliases['nl'] = array(
 	'CategoryTree' => array( 'Categorieboom' ),
 );
+
+/** Swedish (Svenska) */
+$aliases['sv'] = array(
+	'CategoryTree' => array( 'Kategoriträd' ),
+);
+
