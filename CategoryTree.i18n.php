3f74a6d7bc470d545eafd9caa9fb300a42d47428
Raimond Spekking <raymond@users.mediawiki.org>
2010-06-16 19:38:46 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-06-16 19:30:00 UTC)

--- CategoryTree.i18n.php
+++ CategoryTree.i18n.php
@@ -1710,13 +1710,16 @@ $messages['ie'] = array(
  * @author Malafaya
  */
 $messages['io'] = array(
+	'categorytree' => 'Kategorio-arboro',
 	'categorytree-portlet' => 'Kategorii',
+	'categorytree-legend' => 'Montrar kategorio-arboro',
 	'categorytree-category' => 'Kategorio:',
 	'categorytree-go' => 'Montrar Arboro',
 	'categorytree-mode-categories' => 'nur kategorii',
 	'categorytree-mode-pages' => 'pagini ecepte arkivi',
 	'categorytree-mode-all' => 'omna pagini',
 	'categorytree-member-counts' => 'kontenas {{PLURAL:$1|1 subkategorio|$1 subkategorii}}, {{PLURAL:$2|1 pagino|$2 pagini}}, ed {{PLURAL:$3|1 arkivo|$3 arkivi}}',
+	'categorytree-load' => 'kargar',
 	'categorytree-nothing-found' => 'nulo trovita',
 	'categorytree-no-subcategories' => 'nula subkategorii',
 	'categorytree-no-pages' => 'nula pagini o subkategorii',
