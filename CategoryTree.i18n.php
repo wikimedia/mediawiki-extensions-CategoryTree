960d8ff1325835b353acd3ddd8be9ad487ee1811
Raimond Spekking <raymond@users.mediawiki.org>
2010-06-24 20:23:20 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-06-24 20:00:00 UTC)

--- CategoryTree.i18n.php
+++ CategoryTree.i18n.php
@@ -3031,6 +3031,7 @@ Musyariy, kaytaqa AJAX nisqa sapaq JavaScript ruranallawanmi llamk'achiyta atink
  * @author Cin
  * @author KlaudiuMihaila
  * @author Mihai
+ * @author Minisarm
  */
 $messages['ro'] = array(
 	'categorytree' => 'Arborele categoriilor',
@@ -3049,13 +3050,13 @@ $messages['ro'] = array(
 	'categorytree-member-counts' => 'conține {{PLURAL:$1|1 subcategorie|$1 subcategorii}}, {{PLURAL:$2|1 pagină|$2 pagini}} și {{PLURAL:$3|1 fișier|$3 fișiere}}',
 	'categorytree-load' => 'încarcă',
 	'categorytree-loading' => 'încărcare…',
-	'categorytree-nothing-found' => 'nimic găsit',
+	'categorytree-nothing-found' => 'fără rezultat',
 	'categorytree-no-subcategories' => 'nici o subcategorie',
 	'categorytree-no-parent-categories' => 'nici o categorie părinte',
 	'categorytree-no-pages' => 'nici o pagină sau subcategorie',
 	'categorytree-not-found' => 'Categoria <i>$1</i> nu a fost găsită',
 	'categorytree-error' => 'Problemă la încărcarea datelor',
-	'categorytree-retry' => 'Vă rugăm să așteptați o clipă și să încercați din nou.',
+	'categorytree-retry' => 'Vă rugăm să așteptați câteva momente și să încercați din nou.',
 );
 
 /** Tarandíne (Tarandíne)
