b30a06d000c2000804374e7bd364f191668e720b
Raimond Spekking <raymond@users.mediawiki.org>
2011-03-02 20:25:35 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-03-02 20:08:00 UTC)

--- CategoryTree.i18n.php
+++ CategoryTree.i18n.php
@@ -2512,11 +2512,11 @@ $messages['mg'] = array(
  * @author Brest2008
  */
 $messages['mk'] = array(
-	'categorytree' => 'Стебло на категории',
+	'categorytree' => 'Дрво на категории',
 	'categorytree-portlet' => 'Категории',
-	'categorytree-legend' => 'Прикажи стебло на категории',
+	'categorytree-legend' => 'Прикажи дрво на категории',
 	'categorytree-desc' => 'Динамичка навигација низ [[Special:CategoryTree|структурата на категориите]]',
-	'categorytree-header' => 'Внесете назив на категорија за да ја видите нејзината содржина во облик на стебло.
+	'categorytree-header' => 'Внесете назив на категорија за да ја видите нејзината содржина во облик на дрво.
 Да напоменеме дека оваа функција побарува JavaScript т.е. AJAX.
 Ова нема да работи ако имате многу стар прелистувач, или ако имате оневозможено JavaScript.',
 	'categorytree-category' => 'Категорија:',
