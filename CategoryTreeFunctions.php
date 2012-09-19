0aeed5ffeedabf6edcf5905ab1162cd3eb586a6f
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-09-19 12:25:42 -0700

    Fix regression in I331af864: $subcatCount was dropped accidentally.
    Should be cherry picked in 1.20wmf12.
    
    Change-Id: If0a461f8db6a5c8a3b919cf63f2f1f92b74f1534

--- CategoryTreeFunctions.php
+++ CategoryTreeFunctions.php
@@ -808,7 +808,7 @@ class CategoryTree {
 				$attr,
 				wfMessage( 'categorytree-member-num' )
 					// Do not use numParams on params 1-4, as they are only used for customisation.
-					->params( $pages, $fileCount, $allCount, $memberNumsShort )
+					->params( $subcatCount, $pages, $fileCount, $allCount, $memberNumsShort )
 					->escaped()
 			);
 		}
