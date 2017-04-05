42f7e98c8516bca14368b6a307be51e5befcd251
Pierre Boutet <boutetpierre@yahoo.fr>
2017-04-05 15:20:05 +0200

    add Tutoriels display

--- CategoryPageSubclass.php
+++ CategoryPageSubclass.php
@@ -51,6 +51,7 @@ class CategoryTreeCategoryViewer extends CategoryViewer {
 	}
 
 	function clearCategoryState() {
+		$this->articlesTitles = [];
 		$this->child_cats = [];
 		parent::clearCategoryState();
 	}
@@ -61,4 +62,78 @@ class CategoryTreeCategoryViewer extends CategoryViewer {
 		}
 		parent::finaliseCategoryState();
 	}
+
+	/**
+	 * return true if given article is a page using tutorial forms
+	 */
+	private function isTutorial($article) {
+
+	}
+
+
+	/**
+	 * Add a miscellaneous page
+	 * @param Title $title
+	 * @param string $sortkey
+	 * @param int $pageLength
+	 * @param bool $isRedirect
+	 */
+	function addPage( $title, $sortkey, $pageLength, $isRedirect = false ) {
+
+		// we split article in Two parts : those wo are using forms, and those who don't
+
+		if( $title->getNamespace() == NS_MAIN) {
+			// TODO : find other way to realy filter matching pages
+			$this->articlesTitles [] = $title;
+		} else {
+			parent::addPage( $title, $sortkey, $pageLength, $isRedirect );
+		}
+	}
+
+
+
+	/**
+	 * @return string
+	 */
+	function getPagesSection() {
+		global $wgOut;
+
+		$out = '';
+		// print Tutorials parts
+		if(count($this->articlesTitles) > 0) {
+
+			$limit = 4;
+
+			$wgOut->addModules( 'ext.wikifab.wfExplore.js');
+
+			$WfExploreCore = new WfExploreCore();
+
+			$params = array();
+			$params['limit'] = intval($limit);
+			$params['query'] = '[[Category:'.$this->title->getText().']]' ;;
+
+			$WfExploreCore->executeSearch( $request = null , $params);
+
+			$r = "";
+
+			$paramsOutput = [
+					'showPreviousButton' => true,
+					//'noLoadMoreButton' => true,
+					//'replaceClass' => 'exploreQueryResult',
+					'isEmbed' => true
+			];
+			$r .= $WfExploreCore->getSearchResultsHtml($paramsOutput);
+
+			$ti = wfEscapeWikiText( $this->title->getText() );
+
+			$out = "<div id=\"mw-pages\">\n";
+			$out .= '<h2>' . $this->msg( 'category_tutoriels_header', $ti )->parse() . "</h2>\n";
+			$out .= $r;
+			$out .= "\n</div>";
+		}
+
+		// if there is no tutorial, display default category page :
+		return $out . ' ' . parent::getPagesSection();
+	}
+
 }
