fec55f2994c3e8021d0329e45aed510a0062c168
Fomafix <fomafix@googlemail.com>
2018-04-18 08:23:38 +0200

    Simplify by using Xml::element
    Xml::element already makes the HTML encoding.
    
    Change-Id: Idee5e6871c5a7b5e6763ebe85275598b9b217224
    (cherry picked from commit 6684f62bbaa17068c50a0ed89319a515d86bea1c)

--- includes/CategoryTree.php
+++ includes/CategoryTree.php
@@ -517,7 +517,7 @@ class CategoryTree {
 		foreach ( $res as $row ) {
 			$t = Title::newFromRow( $row );
 
-			$label = htmlspecialchars( $t->getText() );
+			$label = $t->getText();
 
 			$wikiLink = $special->getLocalURL( 'target=' . $t->getPartialURL() .
 				'&' . $this->getOptionsAsUrlParameters() );
@@ -527,8 +527,7 @@ class CategoryTree {
 			}
 
 			$s .= Xml::openElement( 'span', [ 'class' => 'CategoryTreeItem' ] );
-			$s .= Xml::openElement( 'a', [ 'class' => 'CategoryTreeLabel', 'href' => $wikiLink ] )
-				. $label . Xml::closeElement( 'a' );
+			$s .= Xml::element( 'a', [ 'class' => 'CategoryTreeLabel', 'href' => $wikiLink ], $label );
 			$s .= Xml::closeElement( 'span' );
 
 			$s .= "\n\t\t";
@@ -587,9 +586,9 @@ class CategoryTree {
 		// configuration setting
 		// patch contributed by Manuel Schneider <manuel.schneider@wikimedia.ch>, Bug 8011
 		if ( $hideprefix ) {
-			$label = htmlspecialchars( $title->getText() );
+			$label = $title->getText();
 		} else {
-			$label = htmlspecialchars( $title->getPrefixedText() );
+			$label = $title->getPrefixedText();
 		}
 
 		$labelClass = 'CategoryTreeLabel ' . ' CategoryTreeLabelNs' . $ns;
@@ -662,8 +661,7 @@ class CategoryTree {
 		}
 		$s .= Xml::tags( 'span', $attr, $bullet ) . ' ';
 
-		$s .= Xml::openElement( 'a', [ 'class' => $labelClass, 'href' => $wikiLink ] )
-			. $label . Xml::closeElement( 'a' );
+		$s .= Xml::element( 'a', [ 'class' => $labelClass, 'href' => $wikiLink ], $label );
 
 		if ( $count !== false && $this->getOption( 'showcount' ) ) {
 			$s .= self::createCountString( RequestContext::getMain(), $cat, $count );
