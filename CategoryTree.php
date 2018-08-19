05650b3d85e5f6c6231df067beefa04cb7a2c959
Fomafix <fomafix@googlemail.com>
2018-08-19 23:23:08 +0200

    Start showing warnings for deprecated PHP entry point
    Change-Id: I1b86936410629ad178f1a76ac73c07cdd518a9aa

--- CategoryTree.php
+++ CategoryTree.php
@@ -15,11 +15,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'CategoryTree' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['CategoryTree'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for CategoryTree extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
 	die( 'This version of the CategoryTree extension requires MediaWiki 1.25+' );
