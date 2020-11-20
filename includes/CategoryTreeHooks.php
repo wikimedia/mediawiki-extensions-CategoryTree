8e493d1ee8b981a8277ac23f3eac758e352875fc
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-11-20 01:00:49 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.10.4
    Change-Id: Iae62c679581802b471dc005b3004d2965e0d96e8

--- includes/CategoryTreeHooks.php
+++ includes/CategoryTreeHooks.php
@@ -154,7 +154,6 @@ class CategoryTreeHooks {
 	) {
 		if ( $parser ) {
 			# flag for use by CategoryTreeHooks::parserOutput
-			// @phan-suppress-next-line PhanUndeclaredProperty
 			$parser->mOutput->mCategoryTreeTag = true;
 		}
 
@@ -188,7 +187,6 @@ class CategoryTreeHooks {
 			// Skip, we've already set the headers unconditionally
 			return;
 		}
-		// @phan-suppress-next-line PhanUndeclaredProperty
 		if ( !empty( $parserOutput->mCategoryTreeTag ) ) {
 			CategoryTree::setHeaders( $outputPage );
 		}
