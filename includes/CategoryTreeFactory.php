df78b94dc0ca7cd64dab386171234c78ba86b76c
Fomafix <fomafix@googlemail.com>
2025-08-21 20:27:19 +0000

    Use PHP8 constructor property promotion syntax in CategoryTreeFactory
    Change-Id: Idc8aec9d895997a0a9d6cfc53ec673186fdbd96a

--- includes/CategoryTreeFactory.php
+++ includes/CategoryTreeFactory.php
@@ -12,24 +12,13 @@ use MediaWiki\Linker\LinkRenderer;
 use Wikimedia\Rdbms\IConnectionProvider;
 
 class CategoryTreeFactory {
-	private Config $config;
-	private Language $contLang;
-	private IConnectionProvider $dbProvider;
-	private LinkBatchFactory $linkBatchFactory;
-	private LinkRenderer $linkRenderer;
-
 	public function __construct(
-		Config $config,
-		Language $contLang,
-		IConnectionProvider $dbProvider,
-		LinkBatchFactory $linkBatchFactory,
-		LinkRenderer $linkRenderer
+		private readonly Config $config,
+		private readonly Language $contLang,
+		private readonly IConnectionProvider $dbProvider,
+		private readonly LinkBatchFactory $linkBatchFactory,
+		private readonly LinkRenderer $linkRenderer,
 	) {
-		$this->config = $config;
-		$this->contLang = $contLang;
-		$this->dbProvider = $dbProvider;
-		$this->linkBatchFactory = $linkBatchFactory;
-		$this->linkRenderer = $linkRenderer;
 	}
 
 	public function newCategoryTree(
